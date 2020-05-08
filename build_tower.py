0b11503094b72f624492635003280fdc57e1ab9d
Caris Moses <carism@mit.edu>
2020-05-07 20:51:31 -0400

    appears to be moving blocks to correct poses but they still fall

--- build_tower.py
+++ build_tower.py
@@ -60,7 +60,7 @@ def main(args):
     # initialize blocks high up so don't collide with floor or eachother
     obj_dims = {}
     obj_goals = {}
-    p_cog_com = {}
+    trans_com_cog = {}
     num_blocks = len(obj_goal_poses)
     block_spread = 0.6
     min_block_y = -block_spread/2
@@ -76,24 +76,35 @@ def main(args):
         block_pos = np.add(block_pos, [0., delta_y, 0.])
         obj_dims[block] = obj_dimensions[urdf_name]
 
-        # obj_goal_poses is the pos of the com, not the center of geometry
-        # need to transform and offset base
+        # obj_goal_poses is the pos of the com
         pos, quat = obj_goal_poses[urdf_name]
-        p_cog_com[block] = p.getDynamicsInfo(block.id, -1)[3]
-        p_goal_block_world = transformation(np.multiply(-1, p_cog_com[block]), pos, quat)
-        pos = np.add(p_goal_block_world, tower_offset)
+        trans_com_cog[block] = compose_matrix(translate=p.getDynamicsInfo(block.id, -1)[3], 
+                                                angles=[0., 0., 0., 1.])
+        #p_goal_block_world = transformation(p_cog_com[block], pos, quat)
+        pos = np.add(pos, tower_offset)
         obj_goals[block] = compose_matrix(translate=pos, angles=euler_from_quaternion(quat))
-        
+
     # sort obj_goals by increasing z height
     sorted_obj_goals_tuple = list((k, v) for k, v in sorted(obj_goals.items(), key=lambda item: item[1][2][3]))
     sorted_obj_goals = OrderedDict()
+    delta_z = 0.0
     for body, pose in sorted_obj_goals_tuple:
+        # for testing
+        #pos = np.add([0., 0., obj_dims[block][2]/2+delta_z], tower_offset)
+        #pose[:3,3] = pos
+        #delta_z += obj_dims[block][2]
+        #
         sorted_obj_goals[body] = pose
+        #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(pose), length=0.5, width=10)        
 
     # let blocks settle
     for t in range(50):
         p.stepSimulation()
-
+        
+    #for i in range(p.getNumJoints(robot.id)):
+    #    pos, quat = p.getLinkState(robot.id, i)[:2]
+    #    vis_frame(pos, quat, lifeTime=0)
+    
     # get transformation of robot hand in EE frame
     p_hand_world, q_hand_world = p.getLinkState(robot.id, 10)[:2]
     trans_hand_world = compose_matrix(translate=p_hand_world, angles=euler_from_quaternion(q_hand_world))
@@ -101,36 +112,71 @@ def main(args):
     trans_ee_world = compose_matrix(translate=p_ee_world, angles=euler_from_quaternion(q_ee_world))
     p_hand_ee = transformation(p_hand_world, p_ee_world, q_ee_world, inverse=True)
     q_hand_ee = quat_math(q_ee_world, q_hand_world, True, False)
-    trans_hand_ee = compose_matrix(translate=p_hand_ee, 
+    # the hand frame is a little inside the hand, want it to be at the point
+    # where the hand and finger meet
+    p_offset = [0., 0., 0.01]
+    trans_hand_ee = compose_matrix(translate=np.add(p_hand_ee, p_offset), 
                                     angles=euler_from_quaternion(q_hand_ee))
-
-    # transformation from robot EE to object center of geometry when grabbed (top down grasp)
+    #trans_hand_world = concatenate_matrices(trans_ee_world, trans_hand_ee)
+    #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(trans_hand_world), length=0.5, width=10)
+    #utils.wait_for_user()
+    
+    # transformation from robot EE to object com when grasped (top down grasp)
     # needed when defining Grab()
-    trans_block_ee = {}
+    trans_com_ee = {}
+    # transformation from robot EE to object cog when grasped (top down grasp)
+    # needed when defining goal pose of EE
+    trans_cog_ee = {}
     for block, dimensions in obj_dims.items():
-        trans_block_hand = compose_matrix(translate=[0., 0., dimensions[2]/2], 
-                                                    angles=[0., 0., 0.])
-        trans_block_ee[block] = concatenate_matrices(trans_block_hand, trans_hand_ee)
-                                                    
-    # transformation of EE in world frame when grasping block
-    grasp_ee_world = {}
+        trans_cog_hand = compose_matrix(translate=[0., 0., dimensions[2]/2], 
+                                        angles=[0., 0., 0.])
+        trans_cog_ee[block] = concatenate_matrices(trans_hand_ee,
+                                                    trans_cog_hand)
+        trans_com_ee[block] = concatenate_matrices(trans_cog_ee[block],
+                                                    trans_com_cog[block])
+        #trans_com_world = concatenate_matrices(trans_ee_world, trans_com_ee[block])
+        #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(trans_com_world), length=0.5, width=10)
+        #utils.wait_for_user()
+                                 
+    # transformation of block cog in world frame (at initial block position)
+    trans_cog_world = {}
     for block, dimensions in obj_dims.items():
-        # get pos of center of geometry
         p_com_world, q_block_world = block.get_pose()
-        p_block_world = transformation(np.multiply(-1, p_cog_com[block]), p_com_world, q_block_world)
-        trans_block_world = compose_matrix(translate=p_block_world,
-                                    angles=euler_from_quaternion(q_block_world))
-        trans_ee_world = concatenate_matrices(trans_block_world, 
-                                                trans_block_ee[block])
+        trans_com_world = compose_matrix(translate=p_com_world, angles=q_block_world)
+        trans_cog_world[block] = concatenate_matrices(trans_com_world, 
+                                                        inverse_matrix(trans_com_cog[block]))
+        #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(trans_cog_world[block]), length=0.5, width=10)
+        #utils.wait_for_user()
+        
+    # transformation of EE in world frame when grasping block (at initial block position)
+    grasp_ee_world_init = {}
+    for block, dimensions in obj_dims.items():
+        trans_ee_world = concatenate_matrices(trans_cog_world[block], 
+                                                trans_cog_ee[block])
         p_ee_world = translation_from_matrix(trans_ee_world)
-        grasp_ee_world[block] = compose_matrix(translate=p_ee_world, 
+        grasp_ee_world_init[block] = compose_matrix(translate=p_ee_world, 
                                                 angles=euler_from_quaternion(q_ee_world))
-
+        #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(grasp_ee_world_init[block]), length=0.5, width=10)
+        #utils.wait_for_user()
+        
+    # transformation of EE in world frame when grasping block (at final block position)
+    grasp_ee_world_final = {}
+    for block, dimensions in obj_dims.items():
+        trans_ee_world = concatenate_matrices(obj_goals[block], 
+                                                trans_cog_ee[block]) # should be the other way around
+        p_ee_world = translation_from_matrix(trans_ee_world)
+        grasp_ee_world_final[block] = compose_matrix(translate=p_ee_world, 
+                                                angles=euler_from_quaternion(q_ee_world))
+        #pb_robot.viz.draw_pose(pb_robot.geometry.pose_from_tform(grasp_ee_world_final[block]), length=0.5, width=10)
+        #utils.wait_for_user()
+        
+    
+        
     # build tower
+    init_q = robot.arm.GetJointValues()
     for block, obj_goal in sorted_obj_goals.items():
         # grasp object
-        grasp_pose = grasp_ee_world[block]
-        newq = robot.arm.ComputeIK(grasp_pose)
+        newq = robot.arm.ComputeIK(grasp_ee_world_init[block])
         robot.arm.hand.Open()
         if newq is not None:
             utils.wait_for_user("Move to desired pose?")
@@ -138,13 +184,13 @@ def main(args):
         else:
             print("Cannot find valid config")
         robot.arm.hand.Close()
-        robot.arm.Grab(block, inverse_matrix(trans_block_ee[block]))
+        robot.arm.Grab(block, inverse_matrix(trans_com_ee[block]))
+
+        #utils.wait_for_user('just grabbed')
+        #robot.arm.SetJointValues(newq)
         
         # move block to tower pose
-        goal_ee_world = concatenate_matrices(obj_goal, trans_block_ee[block])
-        goal_ee_world = compose_matrix(translate=translation_from_matrix(goal_ee_world),
-                                        angles=euler_from_quaternion(q_ee_world))
-        newq = robot.arm.ComputeIK(goal_ee_world)
+        newq = robot.arm.ComputeIK(grasp_ee_world_final[block])
         if newq is not None:
             utils.wait_for_user("Move to desired pose?")
             robot.arm.SetJointValues(newq)
@@ -155,10 +201,14 @@ def main(args):
         utils.wait_for_user("Release obj?")
         robot.arm.Release(block)
         robot.arm.hand.Open()
+        
+    # move back to home configuration
+    robot.arm.SetJointValues(init_q)
 
     utils.wait_for_user("Run sim?")
     for _ in range(1000):
         p.stepSimulation()
+    
     utils.wait_for_user("Done?")
     utils.disconnect()  
 
