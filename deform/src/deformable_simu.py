d9be8400bc56a8cdb15b450452ca759f06e495a1
The-Ripp3r <esr@mit.edu>
2020-08-28 17:38:09 -0400

    cleaned up deformable_sim and added some comments to doc

--- deform/src/deformable_simu.py
+++ deform/src/deformable_simu.py
@@ -3,7 +3,6 @@ from time import sleep
 import pybullet_data
 from math import cos, pi, sin
 
-
 physicsClient = p.connect(p.GUI)
 
 #p.setAdditionalSearchPath(pybullet_data.getD ataPath())
@@ -15,69 +14,43 @@ p.setGravity(0, 0, -10)
 planeOrn = [0,0,0,1]
 planeId = p.loadURDF("geo/plane.urdf", [0,0,0], planeOrn)
 
-#boxId = p.loadURDF("geo/block.urdf", [0,0,2.5], useMaximalCoordinates = True) #no rigid bodies
+#good example of tile on block, commented out clothId uses spring model
+boxId = p.loadURDF("geo/cube.urdf", [0,0,2.5], useMaximalCoordinates = True) #no rigid bodies
+clothId= p.loadURDF("geo/cover_deform.urdf", [-0.5,0.5,2], baseOrientation=[-0.5,-0.5,-0.5,0.5], flags=p.URDF_USE_SELF_COLLISION)
+#clothId = p.loadSoftBody("geo/cover.vtk", basePosition = [-0.5,0.5,2], baseOrientation=[-0.5,-0.5,-0.5,0.5], mass = 1., useNeoHookean = 0, useBendingSprings=1, useMassSpring=1, springElasticStiffness=20, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 0, frictionCoeff = .5, useFaceContact=1)
+p.createSoftBodyAnchor(clothId ,0,boxId,4) #handpicked from meshAPI module and obj file
+p.createSoftBodyAnchor(clothId ,1,boxId,2)
+p.createSoftBodyAnchor(clothId ,2,boxId,3)
+p.createSoftBodyAnchor(clothId ,3,boxId,5)
+p.createSoftBodyAnchor(clothId ,10,boxId,8)
+p.createSoftBodyAnchor(clothId ,14,boxId,9)
+p.createSoftBodyAnchor(clothId ,18,boxId,10)
+p.createSoftBodyAnchor(clothId ,22,boxId,11)
 
-#difference between spring and neohookean
-clothId= p.loadURDF("geo/torus_deform.urdf", [0,0,0], baseOrientation=[sin(pi/4),0,0,cos(pi/4)], flags=p.URDF_USE_SELF_COLLISION)
-#clothId = p.loadSoftBody("cover.vtk", basePosition = [-0.5,-0.5,2], mass = 1., useNeoHookean = 0, useBendingSprings=1,useMassSpring=1, springElasticStiffness=20, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 0, frictionCoeff = .5, useFaceContact=1)
-# p.createSoftBodyAnchor(clothId ,0,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,1,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,2,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,3,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,10,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,14,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,18,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,22,boxId,-1)# [-0.5,-0.5,1])
-
-#clothId= p.loadURDF("geo/torus_deform.urdf", [-0.5,-0.5,2], baseOrientation=[1,0,0,1], flags=p.URDF_USE_SELF_COLLISION)
-#clothId = p.loadSoftBody("cover.vtk", basePosition = [-0.5,-0.5,2], baseOrientation=[1,0,0,cos(0)], mass = 1., useNeoHookean = 0, useBendingSprings=1, useMassSpring=1, springElasticStiffness=20, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 0, frictionCoeff = .5, useFaceContact=1)
-# p.createSoftBodyAnchor(clothId ,0,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,1,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,2,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,3,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,10,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,14,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,18,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,22,boxId,-1)# [-0.5,-0.5,1])
-
-# clothId= p.loadURDF("geo/torus_deform.urdf", [-0.5,-0.5,3], baseOrientation=[0,1,0,cos(0)], flags=p.URDF_USE_SELF_COLLISION)
-# #clothId = p.loadSoftBody("cover.vtk", basePosition = [-0.5,-0.5,2], baseOrientation=[1,0,0,cos(0)], mass = 1., useNeoHookean = 0, useBendingSprings=1, useMassSpring=1, springElasticStiffness=20, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 0, frictionCoeff = .5, useFaceContact=1)
-# p.createSoftBodyAnchor(clothId ,0,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,1,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,2,boxId,-1)# [0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,3,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,10,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,14,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,18,boxId,-1)# [-0.5,-0.5,1])
-# p.createSoftBodyAnchor(clothId ,22,boxId,-1)# [-0.5,-0.5,1])
-
-
-def add_anchors(body, **kwargs):
-  #anchors all 6 pads to block
-  count=0
-  base_pos, base_orientation=p.getBasePositionAndOrientation(boxId)
-  offsets=kwargs['position_offsets']
-  while count<6:
-    dx,dy,dz=offsets.pop()
-    if dy>0:
-      pass
-    #replace clothId with generate urdf that takes in position, orientation, and nehookean param
-    clothId = p.loadSoftBody("cover.vtk", basePosition = [base_pos[0]+dx,base_pos[1]+dy,base_pos[2]+dz], mass = 1., useNeoHookean = 0, useBendingSprings=1,useMassSpring=1, springElasticStiffness=40, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 0, frictionCoeff = .5, useFaceContact=1)
-    p.createSoftBodyAnchor(clothId ,0,boxId,-1)# [0.5,-0.5,1])
-    p.createSoftBodyAnchor(clothId ,1,boxId,-1)# [-0.5,-0.5,1])
-    p.createSoftBodyAnchor(clothId ,2,boxId,-1)# [0.5,-0.5,1])
-    p.createSoftBodyAnchor(clothId ,3,boxId,-1)# [-0.5,-0.5,1])
-
-#anchor demonstration
-# p.createSoftBodyAnchor(clothId  ,0) #removed "extra" parameter
-# p.createSoftBodyAnchor(clothId ,1,-1)
-#boxId2 = p.loadURDF("cube.urdf", [0,0,4.5],useMaximalCoordinates = True) #no rigid bodies
+#print(p.getMeshData(clothId, linkIndex=0))
 
 #deformable demonstration
-#bunny = p.loadURDF("block.urdf", [0,0,1], flags=p.URDF_USE_SELF_COLLISION)
-#bunny2 = p.loadURDF("torus_deform.urdf", [0,0,3], flags=p.URDF_USE_SELF_COLLISION)
+# bunny = p.loadURDF("geo/block.urdf", [0,0,1], flags=p.URDF_USE_SELF_COLLISION)
+# bunny2 = p.loadURDF("geo/torus_deform.urdf", [0,0,3], flags=p.URDF_USE_SELF_COLLISION)
 
-# print(p.getMeshData(clothId, linkIndex=0))
+
+# #IN PROGRESS, will complete if rubber hollow block does not work
+# def add_anchors(body1="geo/cover_deform.urdf", body2="geo/cube.urdf", **kwargs):
+#   #anchors all 6 pads to block
+#   count=0
+#   base_pos, base_orientation=p.getBasePositionAndOrientation(body2)
+#   #hard code quaternions list
+#   #vertices for body2?
+#   mesh_verts=[0,1,2,3,10,14,18,22] #face independent
+#   while count<6:
+#     x,y,z,w=quats[count] #face dependent
+#     vertices=verts[count] #face dependent
+#     #assumes body frames of both bodies are centered and length of body2 is 1m
+#     cover= p.loadURDF(body1, basePosition=[base_pos[0],base_pos[1],base_pos[2]-0.5], baseOrientation=[x,y,z,w], flags=p.URDF_USE_SELF_COLLISION) #baseOrientation= base_orientation*[x,y,z,w] quat math
+#     count2=0
+#     while count<8:
+#       p.createSoftBodyAnchor(cover,mesh_verts[count2],body2,vertices[count2])
+
 
 p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
 p.setRealTimeSimulation(1)
