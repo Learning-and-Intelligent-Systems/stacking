3b0cb4529fcc74ea38b1fd9c4007657628448b02
Caris Moses <carism@mit.edu>
2020-10-01 11:12:17 -0400

    (some) models can now train on visual input

--- learning/train_gat.py
+++ learning/train_gat.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle
 from random import shuffle
+import argparse
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -33,7 +34,7 @@ def get_subsets(data):
             labels.append(label)
     return torch.Tensor(towers), torch.Tensor(labels)
 
-def load_dataset(name):
+def load_dataset(name, args):
     """ Load all the tower data into TensorDatasets. We need a different
     dataset for each tower size, because vectorized Graph Attention Network
     can only ingest batches of graphs with equal numbers of nodes.
@@ -46,13 +47,15 @@ def load_dataset(name):
     """
     with open(f'learning/data/{name}', 'rb') as f:
         all_data = pickle.load(f)
-
+    all_num_blocks = [int(num_blocks[0]) for num_blocks in all_data.keys()]
     datasets = []
-    for num_blocks in range(2, 6):
+    for num_blocks in all_num_blocks:
         data = all_data[f'{num_blocks}block']
         # load the tower data
         towers = torch.Tensor(data['towers'])
         labels = torch.Tensor(data['labels'])
+        if args.visual:
+            images = torch.Tensor(data['images'])
 
         #towers, labels = get_subsets(data)
         # remove the three color channels at the end of each block encoding
@@ -62,9 +65,12 @@ def load_dataset(name):
         # convert absolute xy positions to relative positions
         #towers[:,1:,7:9] -= towers[:,:-1,7:9]
         # add the new dataset to the list of datasets
-        datasets.append(TensorDataset(towers, labels))
+        if args.visual:
+            datasets.append(TensorDataset(towers, labels, images))
+        else:
+            datasets.append(TensorDataset(towers, labels))
 
-    return datasets
+    return datasets, all_num_blocks
 
 def print_split_accuracies(dataset, model):
     """
@@ -108,50 +114,60 @@ def print_split_accuracies(dataset, model):
             print('Geom %d, CoM %d: %f' % (g, c, acc))
 
 
-def train(model, datasets, test_datasets):
+def train(model, datasets, test_datasets, args):
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    batch_size = 32
-    losses = []
+    train_losses = []
     num_data_points = len(datasets[0])
+    epoch_ids, test_accuracies = [], []
 
-    for epoch_idx in range(100):
+    for epoch_idx in range(args.epochs):
         # create a dataloader for each tower size
         iterable_dataloaders = [
-            iter(DataLoader(d, batch_size=batch_size, shuffle=True))
+            iter(DataLoader(d, batch_size=args.batch_size, shuffle=True))
             for d in datasets]
         accs = [[],[],[],[]]
-        for batch_idx in range(num_data_points // batch_size):
+        for batch_idx in range(num_data_points // args.batch_size):
             # shuffle(iterable_dataloaders)
             # iterate through the tower sizes in the inner loop
             for dx, iterable_dataloader in enumerate(iterable_dataloaders):
                 
                 optimizer.zero_grad()
-                towers, labels = next(iterable_dataloader)
+                if args.visual:
+                    towers, labels, images = next(iterable_dataloader)
+                else:
+                    towers, labels = next(iterable_dataloader)
                 if torch.cuda.is_available():
                     towers = towers.cuda()
                     labels = labels.cuda()
-                preds = model.forward(towers, k=towers.shape[1]-1)
+                    if args.visual:
+                        images = images.cuda()
+                if args.visual:
+                    preds = model.forward(images, k=towers.shape[1]-1)
+                else:
+                    preds = model.forward(towers, k=towers.shape[1]-1)
                 l = F.binary_cross_entropy(preds, labels)
                 l.backward()
                 optimizer.step()
                 accuracy = ((preds>0.5) == labels).float().mean()
                 accs[dx].append(accuracy.item())
-                losses.append(np.mean(accs[dx][-500:]))
+                train_losses.append(np.mean(accs[dx][-500:]))
             
                 #print(preds, labels)
                 
 
             if batch_idx % 40 == 0:
-                print(f'Epoch {epoch_idx}\tBatch {batch_idx}:\t {losses[-4:]}')
+                print(f'Epoch {epoch_idx}\tBatch {batch_idx}:\t {train_losses[-4:]}')
 
         if epoch_idx % 5 == 0:
-            accuracies = test(model, test_datasets, fname='lstm_preds.pkl')
+            epoch_ids += [epoch_idx]
+            accuracies = test(model, test_datasets, args, fname='lstm_preds.pkl')
+            test_accuracies.append(accuracies)
             print('Val:', accuracies)
 
         #print_split_accuracies(datasets[0], model)
-    return losses
+    return train_losses, epoch_ids, test_accuracies
 
-def test(model, datasets, fname=''):
+def test(model, datasets, args, fname=''):
     accuracies = []
     
     results = []
@@ -160,11 +176,18 @@ def test(model, datasets, fname=''):
         # pull out the input and output tensors for the whole dataset
         towers = dataset[:][0]
         labels = dataset[:][1]
+        if args.visual:
+            images = dataset[:][2]
         if torch.cuda.is_available():
             towers = towers.cuda()
             labels = labels.cuda()
+            if args.visual:
+                images = images.cuda()
         # run the model on everything
-        preds = model.forward(towers, k=towers.shape[1]-1)
+        if args.visual:
+            preds = model.forward(images, k=towers.shape[1]-1)
+        else:
+            preds = model.forward(towers, k=towers.shape[1]-1)
         # calculate the and save the accuracy
         accuracy = ((preds>0.5) == labels).float().mean()
         accuracies.append(accuracy.item())
@@ -176,28 +199,44 @@ def test(model, datasets, fname=''):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # NOTE(caris): this has only been tested for the GatedGN network!!
+    parser.add_argument('--visual', action='store_true') 
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--batch-size', default=32, type=int)
+    parser.add_argument('--epochs', default=100, type=int)
+    args = parser.parse_args()
+    
+    if args.debug:
+        import pdb; pdb.set_trace()
+    
     # the number of hidden variables in the graph NN
     M = 64
     #model = FCGAT(14+M, M)
     #model = MLP(5, 256)
-    model = FCGN(14, 64)
-    #model = TowerLSTM(14, 32)
-    #model = GatedGN(14, 32)
+    #model = FCGN(14, 64)
+    model = TowerLSTM(14, 32, visual=args.visual, image_dim=150)
+    #model = GatedGN(14, 32, visual=args.visual, image_dim=150)
     if torch.cuda.is_available():
         model = model.cuda()
 
-    train_datasets = load_dataset('random_blocks_(x40000)_5blocks_all.pkl')
-    test_datasets = load_dataset('random_blocks_(x2000)_5blocks_all.pkl')
-    #train_datasets = load_dataset('random_blocks_(x5000)_5blocks_pwu.pkl')
-    losses = train(model, train_datasets, test_datasets)
-    plt.plot(losses)
+    #train_datasets = load_dataset('random_blocks_(x20000)_2blocks_all.pkl', args)
+    train_datasets, _ = load_dataset('random_blocks_(x16)_2blocks_all.pkl', args)
+    test_datasets, num_test_blocks = load_dataset('random_blocks_(x16)_2blocks_all.pkl', args)
+    
+    train_losses, epoch_ids, test_accuracies = train(model, train_datasets, test_datasets, args)
+    plt.plot(train_losses)
+    plt.title('Training Loss')
     plt.xlabel('Batch (x10)')
     plt.show()
 
-    #test_datasets = load_dataset('random_blocks_(x2000)_quat.pkl')
-    accuracies = test(model, test_datasets, fname='lstm_preds.pkl')
-    print(accuracies)
-    plt.scatter(np.arange(2,6), accuracies)
-    plt.xlabel('Num Blocks in Tower')
+    final_accuracies = test(model, test_datasets, args, fname='lstm_preds.pkl')
+    #print(accuracies)
+    plt.plot(epoch_ids+[args.epochs], test_accuracies+[final_accuracies], label=num_test_blocks)
+    #plt.scatter(np.arange(2,3), accuracies)
+    #plt.xlabel('Num Blocks in Tower')
+    plt.legend(title='number of blocks')
+    plt.title('Test Accuracy')
+    plt.xlabel('Epoch ID')
     plt.ylabel('Accuracy')
     plt.show()
