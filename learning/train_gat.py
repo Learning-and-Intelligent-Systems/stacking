f2e034afb8531edb8255d0e02575485614ad44e0
Caris Moses <carism@mit.edu>
2020-10-06 10:47:00 -0400

    using larger datasets

--- learning/train_gat.py
+++ learning/train_gat.py
@@ -220,8 +220,8 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         model = model.cuda()
 
-    train_dataset = 'random_blocks_(x24)_2blocks_uniform_density.pkl'
-    test_dataset = 'random_blocks_(x24)_2blocks_uniform_density.pkl'
+    train_dataset = 'random_blocks_(x20000)_2blocks_uniform_density.pkl'
+    test_dataset = 'random_blocks_(x2000)_2blocks_uniform_density.pkl'
     train_datasets, _ = load_dataset(train_dataset, args)
     test_datasets, num_test_blocks = load_dataset(test_dataset, args)
     
