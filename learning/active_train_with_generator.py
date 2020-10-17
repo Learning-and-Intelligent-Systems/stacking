cfcd26e1f0fa8f19b1c58185c8b7530191dba869
Izzy Brand <izzybrand@gmail.com>
2020-10-16 20:40:11 -0400

    implement reset function in active learning

--- learning/active_train_with_generator.py
+++ learning/active_train_with_generator.py
@@ -3,7 +3,7 @@ Massachusetts Institute of Technology
 
 Izzy Brand, 2020
 """
-
+from copy import deepcopy
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
@@ -64,10 +64,11 @@ def mc_dropout_score(model, x, k=100):
         # model predicts probility of stable p, so the distribution is p, 1-p
         return bernoulli_bald(p)
 
-def active(model, train_datasets, test_datasets):
+def active(model, train_datasets, test_datasets, score_func=mc_dropout_score):
     batch_size = 32
     num_to_acquire_per_batch = 4
-    while True:
+    num_to_acquire_in_total = 40
+    for _ in range(num_to_acquire_per_batch // num_to_acquire_in_total1)
         model.train(True)
 
         for i in range(4):
@@ -95,9 +96,30 @@ def active(model, train_datasets, test_datasets):
         print('Dataset sizes:', [len(d) for d in train_datasets])
         train(model, train_datasets, test_datasets, epochs=1)
 
+
+def evaluate_active_learning_performance(model, score_func, train_datasets, test_datasets):
+
+    # pretrain the model
+    losses = train(model, train_datasets, test_datasets, epochs=1)
+    # plt.plot(losses)
+    # plt.xlabel('Batch (x10)')
+    # plt.show()
+
+    # do active learning until the pool is empty
+    scores = active(model, train_datasets, test_datasets, score_func=score_func)
+    # import sys; sys.exit(0)
+
+    # and do a final test of the model
+    accuracies = test(model, test_datasets, fname='lstm_preds.pkl')
+    print(accuracies)
+    plt.scatter(np.arange(2,6), accuracies)
+    plt.xlabel('Num Blocks in Tower')
+    plt.ylabel('Accuracy')
+    plt.show()
+
 if __name__ == '__main__':
     model = DropoutFCGN(14, 64)
-    model.backup()
+    
     num_train = 1000
 
     if torch.cuda.is_available():
@@ -112,21 +134,8 @@ if __name__ == '__main__':
     train_datasets = [TensorDataset(t.tensors[0][subset_idxs], t.tensors[1][subset_idxs])\
         for t in train_datasets]
 
+    for model_class, score_func in [(DropoutFCGN(14, 64), mc_dropout_score),
+                                    (DropoutFCGN(14, 64), torch.rand_like)]:
 
-    # pretrain the model
-    losses = train(model, train_datasets, test_datasets, epochs=1)
-    # plt.plot(losses)
-    # plt.xlabel('Batch (x10)')
-    # plt.show()
-
-    # do active learning until the pool is empty
-    scores = active(model, train_datasets, test_datasets)
-    # import sys; sys.exit(0)
-
-    # and do a final test of the model
-    accuracies = test(model, test_datasets, fname='lstm_preds.pkl')
-    print(accuracies)
-    plt.scatter(np.arange(2,6), accuracies)
-    plt.xlabel('Num Blocks in Tower')
-    plt.ylabel('Accuracy')
-    plt.show()
+        model.backup()
+        evaluate_active_learning_performance(model, score_func, deepcopy(train_datasets), deepcopy(test_datasets))
