4d45d693e10e18822717171c79ad038f712c4a26
Michael Noseworthy <mnoseworthy7@gmail.com>
2020-10-29 11:18:01 -0400

    Update train_graph_net code to use new dataset structure

--- learning/train_graph_net.py
+++ learning/train_graph_net.py
@@ -12,13 +12,14 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils.data import TensorDataset, DataLoader
 
-from learning.analyze_data import is_geometrically_stable, is_com_stable, get_geometric_thresholds
-from learning.gat import FCGAT
-from learning.gn import FCGN
-from learning.mlp import MLP
-from learning.lstm import TowerLSTM
-from learning.gated_gn import GatedGN
-from learning.topdown_net import TopDownNet
+from learning.domains.towers.analyze_data import is_geometrically_stable, is_com_stable, get_geometric_thresholds
+from learning.domains.towers.tower_data import TowerDataset, TowerSampler
+from learning.models.gat import FCGAT
+from learning.models.gn import FCGN
+from learning.models.mlp import MLP
+from learning.models.lstm import TowerLSTM
+from learning.models.gated_gn import GatedGN
+from learning.models.topdown_net import TopDownNet
 
 def get_subsets(data):
     towers, labels = [], []
@@ -36,7 +37,7 @@ def get_subsets(data):
 
 
 
-def load_dataset(name, K=1):
+def load_dataset(name, augment=False, K=1):
     """ Load all the tower data into TensorDatasets. We need a different
     dataset for each tower size, because vectorized Graph Attention Network
     can only ingest batches of graphs with equal numbers of nodes.
@@ -50,19 +51,8 @@ def load_dataset(name, K=1):
     with open(f'learning/data/{name}', 'rb') as f:
         all_data = pickle.load(f)
 
-    datasets = []
-    for num_blocks in range(2, 6):
-        data = all_data[f'{num_blocks}block']
-        # load the tower data
-        towers = torch.Tensor(data['towers'])
-        labels = torch.Tensor(data['labels'])
+    return TowerDataset(all_data, augment=augment, K_skip=K)
 
-        #towers, labels = get_subsets(data)
-        towers = preprocess(towers)
-        # add the new dataset to the list of datasets
-        datasets.append(TensorDataset(towers[::K,:], labels[::K]))
-
-    return datasets
 
 def print_split_accuracies(dataset, model):
     """
@@ -106,73 +96,68 @@ def print_split_accuracies(dataset, model):
             print('Geom %d, CoM %d: %f' % (g, c, acc))
 
 
-def train(model, datasets, test_datasets=None, epochs=100, is_ensemble=False):
+def train(model, dataset, test_dataset=None, epochs=100, is_ensemble=False):
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     batch_size = 32
     losses = []
-    num_data_points = len(datasets[0])
     model.training = True
+
+    train_loader = DataLoader(dataset=dataset,
+                              batch_sampler=TowerSampler(dataset=dataset,
+                                                         batch_size=batch_size,
+                                                         shuffle=True))
 
     for epoch_idx in range(epochs):
         # create a dataloader for each tower size
-        iterable_dataloaders = [
-            iter(DataLoader(d, batch_size=batch_size, shuffle=True))
-            for d in datasets]
-        accs = [[],[],[],[]]
-        for batch_idx in range(num_data_points // batch_size):
-            # shuffle(iterable_dataloaders)
-            # iterate through the tower sizes in the inner loop
-            for idx, iterable_dataloader in enumerate(iterable_dataloaders):
-                model.train(True)
-                optimizer.zero_grad()
-                catch = next(iterable_dataloader)
-                # print(catch)
-                towers, labels =catch
-                if torch.cuda.is_available():
-                    towers = towers.cuda()
-                    labels = labels.cuda()
-                preds = model.forward(towers, k=towers.shape[1]-1)
+        accs = {2: [], 3: [], 4: [], 5: []}
+        for batch_idx, (towers, labels) in enumerate(train_loader):
+            model.train(True)
+            optimizer.zero_grad()
+            if torch.cuda.is_available():
+                towers = towers.cuda()
+                labels = labels.cuda()
+            preds = model.forward(towers, k=towers.shape[1]-1)
 
-                if is_ensemble:
-                    # NOTE(izzy): do I run into any weird gradient magnitude issues if
-                    # i combine the losses for every model in the ensemble? Pretty
-                    # sure I shuold use sum, because mean would reduce the magnitude
-                    # of the gradient for each model
-                    labels = labels[:, None, ...].expand(-1, model.ensemble_size)
-                    scale = model.ensemble_size
-                else:
-                    scale = 1.
+            if is_ensemble:
+                # NOTE(izzy): do I run into any weird gradient magnitude issues if
+                # i combine the losses for every model in the ensemble? Pretty
+                # sure I shuold use sum, because mean would reduce the magnitude
+                # of the gradient for each model
+                labels = labels[:, None, ...].expand(-1, model.ensemble_size)
+                scale = model.ensemble_size
+            else:
+                scale = 1.
 
-                l = F.binary_cross_entropy(preds, labels) * scale
-                l.backward()
-                optimizer.step()
+            l = F.binary_cross_entropy(preds, labels) * scale
+            l.backward()
+            optimizer.step()
 
-                accuracy = ((preds>0.5) == labels).float().mean()
-                accs[idx].append(accuracy.item())
-                losses.append(np.mean(accs[idx][-500:]))
+            n_blocks = towers.shape[1]
+            accuracy = ((preds>0.5) == labels).float().mean()
+            accs[n_blocks].append(accuracy.item())
+            losses.append(np.mean(accs[n_blocks][-500:]))
 
 
             if batch_idx % 40 == 0:
                 print(f'Epoch {epoch_idx}\tBatch {batch_idx}:\t {losses[-4:]}')
 
-        if test_datasets is not None and epoch_idx % 5 == 0:
+        if test_dataset is not None and epoch_idx % 5 == 0:
             model.train(False)
-            accuracies = test(model, test_datasets, is_ensemble=is_ensemble)
+            accuracies = test(model, test_dataset, is_ensemble=is_ensemble)
             print('Val:', accuracies)
 
-        #print_split_accuracies(datasets[0], model)
     return losses
 
-def test(model, datasets, fname='', is_ensemble=False):
+def test(model, dataset, fname='', is_ensemble=False):
     model.train(False) # turn off dropout before computing accuracy
     accuracies = []
 
     results = []
     # iterate through the tower sizes
-    for dataset in datasets:
+    for k in range(2, 6):
         # pull out the input and output tensors for the whole dataset
-        towers = dataset[:][0]
-        labels = dataset[:][1]
+        towers = dataset.tower_tensors['%dblock' % k]
+        labels = dataset.tower_labels['%dblock' % k]
         if torch.cuda.is_available():
             towers = towers.cuda()
             labels = labels.cuda()
@@ -205,21 +190,21 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         model = model.cuda()
 
-    train_datasets = load_dataset('random_blocks_(x40000)_5blocks_all.pkl')
+    train_dataset = load_dataset('random_blocks_(x40000)_5blocks_all.pkl', augment=True, K=100)
     # train_datasets = load_dataset('random_blocks_(x40000)_5blocks_uniform_mass_aug_4.pkl', K=1)
     print('Number of Training Towers')
-    for d in train_datasets:
-        print(len(d))
-    test_datasets = load_dataset('random_blocks_(x2000)_5blocks_all.pkl')
+    print(len(train_dataset))
+
+    test_dataset = load_dataset('random_blocks_(x2000)_5blocks_all.pkl', augment=False)
     # test_datasets = load_dataset('random_blocks_(x2000)_5blocks_uniform_mass.pkl', K=1)
     #train_datasets = load_dataset('random_blocks_(x5000)_5blocks_pwu.pkl')
-    losses = train(model, train_datasets, test_datasets, epochs=500)
+    losses = train(model, train_dataset, test_dataset, epochs=500)
     plt.plot(losses)
     plt.xlabel('Batch (x10)')
     plt.show()
 
     #test_datasets = load_dataset('random_blocks_(x2000)_quat.pkl')
-    accuracies = test(model, test_datasets, fname='lstm_preds.pkl')
+    accuracies = test(model, test_dataset, fname='lstm_preds.pkl')
     print(accuracies)
     plt.scatter(np.arange(2,6), accuracies)
     plt.xlabel('Num Blocks in Tower')
