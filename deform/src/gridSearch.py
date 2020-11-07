8c3dd253c3298ca3ef7b991e35c94300f62492f4
The-Ripp3r <esr@mit.edu>
2020-11-06 19:21:21 -0500

    added cmd line support but still needs saving support

--- deform/src/gridSearch.py
+++ deform/src/gridSearch.py
@@ -1,19 +1,10 @@
 import pybullet as p
 from odio_urdf import *
 import os
+import sys
 import pybullet_data
+import time
 
-physicsClient = p.connect(p.GUI)
-p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
-p.setGravity(0, 0, -10)
-p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
-
-planeOrn = [0,0,0,1]
-planeId = p.loadURDF("geo/plane_base.urdf", [0,0,0], planeOrn)
-
-num_blocks=2
-num_steps=2000
-approved_combos=[] # tuples of (mu, lambda, damping, friction, and inner mass of second block)
 
 #constants: 
 # -geo of all parts
@@ -23,13 +14,27 @@ approved_combos=[] # tuples of (mu, lambda, damping, friction, and inner mass of
 # -mass ratio of cover to inner is 1:1.6 for bottom block 
 #   (comes from cover needing to be at least 1 and inner being 60% more in volume)
 
-#for loops for mu, lambda, damping, friction, inner mass of second block [1,2]
-m=570
-l=570
-d=0.01
-f=0.5
-mass=1.6
+#ranges:
+#m: [90, 570] step=5?
+#l: [200, 570] step=5?
+#d: [0.01, 0.09] step=0.005
+#f: [0, 1] step=0.05
+#mass: [1, 2.6] step=0.05 
 
+#SETUP
+physicsClient = p.connect(p.DIRECT)
+p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
+p.setGravity(0, 0, -10)
+p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
+planeOrn = [0,0,0,1]
+planeId = p.loadURDF("geo/plane_base.urdf", [0,0,0], planeOrn)
+num_blocks=2
+num_steps=2000
+
+
+#######CODE########
+#filename for cover, filename for upper inner block, mu, lambda, damping, friction, mass of second block
+filename1, filename2, m, l, d, f, mass = sys.argv[1:]
 #generate urdfs
 cover = Robot(Deformable(
             Inertial(
@@ -70,9 +75,9 @@ mut_block = Robot(Link(
             ), name="mut_block")
 
 #save urdfs
-with open('geo/mutable_flex.urdf', 'w') as handle:
+with open(filename1, 'w') as handle:
         handle.write(str(cover))
-with open('geo/mutable_block.urdf', 'w') as handle:
+with open(filename2, 'w') as handle:
         handle.write(str(mut_block))
 
 
@@ -80,11 +85,11 @@ with open('geo/mutable_block.urdf', 'w') as handle:
 blocks=[]
 for i in range(num_blocks):
     if i==num_blocks-1:
-      innerbox=p.loadURDF("geo/mutable_block.urdf", [0.5,0.5,0.5+i*1], useMaximalCoordinates = True)
+      innerbox=p.loadURDF(filename2, [0.5,0.5,0.5+i*1], useMaximalCoordinates = True)
     else:
       innerbox=p.loadURDF("geo/block.urdf", [0.5,0.5,0.5+i*1], useMaximalCoordinates = True)
     blocks.append(innerbox)
-    hollow= p.loadURDF("geo/mutable_flex.urdf",[0,0,0+i*1], flags=p.URDF_USE_SELF_COLLISION)
+    hollow= p.loadURDF(filename1,[0,0,0+i*1], flags=p.URDF_USE_SELF_COLLISION)
 
 #step simulation and record last known locations
 last_loc=[]
@@ -94,6 +99,7 @@ for i in range(num_steps):
       for block in blocks:
         last_loc.append(p.getBasePositionAndOrientation(block)[0][2])
 
+#Save if stable
 unstable=False
 for loc in last_loc:
   if loc<0:
@@ -102,4 +108,4 @@ for loc in last_loc:
     break
 if not unstable:
   print("SUCCESS")
-  approved_combos.append((m,l,d,f,mass))
+  #SAVE SOMEHOW
