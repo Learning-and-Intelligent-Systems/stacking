improved reward calculation and fixing terminal node marking

planning/problems.py
@@ -7,6 +7,7 @@ import torch
 from block_utils import ZERO_POS, Pose
 from planning.utils import make_tower_dataset, random_placement
 from planning.tree import NodeValue
+from tower_planner import TowerPlanner
 
 class Problem:
     def __init__(self):
@@ -15,13 +16,14 @@ class Problem:
     def sample_actions(self, node_value, model):
         pass
         
-    def cost_fn(self, towers, model):
+    def reward_fn(self, towers, model):
         pass
         
 class Tallest(Problem):
-    def __init__(self):
+    def __init__(self, max_height):
         self.samples_per_block = 5
-        self.max_height = 5
+        self.max_height = max_height
+        self.tp = TowerPlanner(stability_mode='contains')
         
     def sample_actions(self, node_value, model):
         new_values = []
@@ -44,18 +46,24 @@ class Tallest(Problem):
                     new_values.append(NodeValue(new_tower, new_blocks_remaining))
                     eval_towers.append(new_tower)
                     
-        costs = self.cost_fn(eval_towers, model)
-        if len(eval_towers[0]) == self.max_height:
-            leaves = [True]*len(eval_towers)
-        else:
-            leaves = [False]*len(eval_towers)
-        return new_values, costs, leaves
+        all_rewards = self.reward_fn(eval_towers, model)
 
-    def cost_fn(self, towers, model):
+        terms = [False]*len(eval_towers)
+        for i, tower in enumerate(eval_towers):
+            # rewards of 0 are unstable --> terminal nodes
+            # once max height is reached --> terminal nodes
+            if all_rewards['exp_reward'][i] == 0 or len(tower) == self.max_height:
+                terms[i] = [True]
+        return new_values, all_rewards, terms
+
+    def reward_fn(self, towers, model):
+        all_rewards = {'exp_reward': [], 'reward': [], 'ground_truth': []}
         if len(towers[0]) == 1:
-            # only single block towers, always stable, but use 0 so all 
-            # equally likely to get selected
-            costs = np.zeros((len(towers)))
+            # only single block towers, always stable
+            reward = [tower[0].dimensions[2] for tower in towers]
+            all_rewards['exp_reward'] = reward
+            all_rewards['reward'] = reward
+            all_rewards['ground_truth'] = reward
         else:
             tower_loader = make_tower_dataset(towers)
             # this assumes there is only one batch
@@ -63,14 +71,22 @@ class Tallest(Problem):
                 with torch.no_grad():
                     preds = model.forward(tensor)
             p_stables = preds.mean(dim=1) # average ensemble output
-            costs = []
+            
+            exp_rewards = []
+            rewards = []
+            ground_truths = []
             for ix, (p, tower) in enumerate(zip(p_stables, towers)):
-                if p > 0.5: # TODO: stable?
-                    tower_height = np.sum([block.dimensions[2] for block in tower])
-                    costs += [float(p*tower_height)]
+                tower_height = np.sum([block.dimensions[2] for block in tower])
+                rewards += [tower_height]
+                if p > 0.5: # stable
+                    exp_rewards += [float(p*tower_height)]
                 else:
-                    costs += [0]
-        return costs
+                    exp_rewards += [0]
+                ground_truths += [self.tp.tower_is_constructable(tower)*tower_height]
+            all_rewards['exp_reward'] = exp_rewards
+            all_rewards['reward'] = rewards
+            all_rewards['ground_truth'] = ground_truths
+        return all_rewards
 
 class Overhang(Problem):
     def __init__(self):
@@ -79,7 +95,7 @@ class Overhang(Problem):
     def sample_actions(self, node_value, model):
         pass
         
-    def cost_fn(self, towers, model):
+    def reward_fn(self, towers, model):
         pass
             
 class Deconstruct(Problem):
@@ -89,5 +105,5 @@ class Deconstruct(Problem):
     def sample_actions(self, node_value, model):
         pass
         
-    def cost_fn(self, towers, model):
+    def reward_fn(self, towers, model):
         pass

planning/tree.py
@@ -1,24 +1,48 @@
 from collections import namedtuple
+import numpy as np
 
-Node = namedtuple('Node', ['parent', 'children', 'leaf', 'value', 'cost'])
+Node = namedtuple('Node', ['parent', 'children', 'term', 'value', 'exp_reward', 'reward', 'ground_truth'])
 NodeValue = namedtuple('NodeValue', ['tower', 'blocks_remaining'])
 
 class Tree:
     def __init__(self, init_value):
-        self.nodes = {0: Node(None, [], False, init_value, -1)}
+        self.nodes = {0: Node(None, [], False, init_value, 0, 0, 0)}
         self.count = 1
         
-    def expand(self, value, cost, parent, leaf):
-        self.nodes[self.count] = Node(parent, [], leaf, value, cost)
+    def expand(self, value, exp_reward, reward, ground_truth, parent, term):
+        self.nodes[self.count] = Node(parent, [], term, value, exp_reward, reward, ground_truth)
         self.count += 1
         
-    def get_best_node(self, expand=True):
+    def get_next_node(self):
+        node_rewards = np.array([node.exp_reward for _, node in self.nodes.items()])
+        # if no node has a reward then they all have equal prob of being selected
+        if sum(node_rewards) == 0:
+            node_rewards = np.ones(len(node_rewards))
+        norm_node_rewards = node_rewards/np.sum(node_rewards)
+        valid_next_node = False
+        while not valid_next_node:
+            next_node_i = np.random.choice(list(self.nodes.keys()), p=norm_node_rewards)
+            next_node_id = list(self.nodes.keys())[next_node_i]
+            if not self.nodes[next_node_id].term:
+                valid_next_node = True
+        return next_node_id
+    
+    def get_exp_best_node(self, height):
         best_node = None
-        best_cost = -1
+        best_exp_reward = -1
         for node in self.nodes:
-            if self.nodes[node].cost >= best_cost:
-                if (expand and not self.nodes[node].leaf) or (not expand):
-                    best_node = node
-                    best_cost = self.nodes[node].cost
-        return best_node, self.nodes[best_node].value
+            if (self.nodes[node].exp_reward >= best_exp_reward) and \
+                    (len(self.nodes[node].value.tower) == height):
+                best_node = node
+                best_exp_reward = self.nodes[node].exp_reward
+        return best_node
         
+    def get_ground_truth_best_node(self, height):
+        best_node = None
+        best_gt_reward = -1
+        for node in self.nodes:
+            if (self.nodes[node].ground_truth >= best_gt_reward) and \
+                    (len(self.nodes[node].value.tower) == height):
+                best_node = node
+                best_gt_reward = self.nodes[node].ground_truth
+        return best_node