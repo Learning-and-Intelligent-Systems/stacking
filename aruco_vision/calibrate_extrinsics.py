6f9a5e53d6f53e6796e862761376e632483e6582
Izzy Brand <izzybrand@gmail.com>
2021-01-22 21:16:08 -0500

    change batch pose multiplication to einsum

--- aruco_vision/calibrate_extrinsics.py
+++ aruco_vision/calibrate_extrinsics.py
@@ -13,7 +13,7 @@ class ExtrinsicsError(nn.Module):
 
     def reset(self):
         self.xyz_rpy_BC = nn.Parameter(torch.randn(6))
-        self.xyz_rpy_GO = nn.Parameter(torch.randn(6))
+        self.xyz_rpy_GO = nn.Parameter(torch.randn(6)/100)
 
     def euler_to_rotation_matrix(self, rpy):
         c = torch.cos
@@ -35,35 +35,27 @@ class ExtrinsicsError(nn.Module):
     def forward(self, X_BG, X_CO):
         X_GO = self.get_pose(self.xyz_rpy_GO)
         X_BC = self.get_pose(self.xyz_rpy_BC)
-        return (X_BC @ X_CO - X_BG @ X_GO).pow(2).mean()
+        X_BCO = torch.einsum('ij,njk->nik', X_BC, X_CO)
+        X_BGO = torch.einsum('nij,jk->nik', X_BG, X_GO)
+        return (X_BCO - X_BGO).pow(2).mean()
 
 
 def train(model, dataset):
-    best_loss = None
-    best_pose = None
-    for _ in range(100):
-        optimizer = optim.Adam(model.parameters(), lr=1e-5)
-        data_loader = DataLoader(dataset=dataset, batch_size=16, shuffle=True)
-        model.reset()
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
+    data_loader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
+    loss = 0
+    for epoch in range(10):  # loop over the dataset multiple times
 
-        loss = 0
-        for epoch in range(5):  # loop over the dataset multiple times
+        for i, (X_BG, X_CO) in enumerate(data_loader, 0):
+            optimizer.zero_grad()
+            loss = model(X_BG, X_CO)
+            loss.backward()
+            optimizer.step()
 
-            for i, (X_BG, X_CO) in enumerate(data_loader, 0):
-                optimizer.zero_grad()
-                loss = model(X_BG, X_CO)
-                loss.backward()
-                optimizer.step()
+    return loss.item()
 
-        print(f'Final Loss: {loss.item()}')
-        if best_loss is None:
-            best_loss = loss
-        elif loss < best_loss:
-            best_loss = loss
-            best_poses = (model.xyz_rpy_BC.detach(), model.xyz_rpy_GO.detach())
-            # print(f'Epoch {epoch}.{i} loss: {loss.item()}')
 
-    print(best_poses)
+
 
 def get_synthetic_data(N=100):
     from scipy.spatial.transform import Rotation
@@ -92,8 +84,8 @@ def object_pose_message_to_X_CO(message):
 def load_rosbag_data(filename):
     # get a panda for forward kinematics
     pb_robot.utils.connect(use_gui=False)
-    panda = pb_robot.panda.Panda() 
-    
+    panda = pb_robot.panda.Panda()
+
     X_COs = []
     X_BGs = []
     prev_joint_states_message = None
@@ -106,7 +98,7 @@ def load_rosbag_data(filename):
             X_BG = panda.arm.ComputeFK(prev_joint_states_message.message.position[:7])
             time_difference = np.abs(message.timestamp.to_sec() \
                 - prev_joint_states_message.timestamp.to_sec())
-            
+
             if time_difference < 0.01:
                 X_COs.append(X_CO)
                 X_BGs.append(X_BG)
@@ -115,15 +107,29 @@ def load_rosbag_data(filename):
             prev_joint_states_message = message
 
     bag.close()
-    pb_robot.utils.disconnect()  
+    pb_robot.utils.disconnect()
     return np.array(X_BGs), np.array(X_COs)
 
 
 
 if __name__ == '__main__':
     # X_BG, X_CO = get_synthetic_data(N=1000)
+    print('Loading rosbag')
     X_BG, X_CO = load_rosbag_data('aruco_vision/cal.bag')
-    print(X_BG.shape)
+    print(f'Got {X_BG.shape[0]} pairs.')
     dataset = TensorDataset(torch.Tensor(X_BG), torch.Tensor(X_CO))
     model = ExtrinsicsError()
-    train(model, dataset)
+
+
+    best_loss = None
+    best_poses = None
+    for i in range(100):
+        model.reset()
+        loss = train(model, dataset)
+
+        print(f'Loss {i+1}/{100}: {loss}')
+        if best_loss is None or loss < best_loss:
+            best_loss = loss
+            best_poses = (model.xyz_rpy_BC.detach(), model.xyz_rpy_GO.detach())
+
+    print(best_loss, best_poses)
