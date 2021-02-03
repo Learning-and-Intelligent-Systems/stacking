10cc3e5fd9881cd09fde1d324866365c332655c9
Caris Moses <carism@mit.edu>
2021-02-03 12:04:14 -0500

    adding block set file for 3D printed blocks, and converting to meters and kg for pybullet

--- learning/domains/towers/create_block_set_file.py
+++ learning/domains/towers/create_block_set_file.py
@@ -1,6 +1,7 @@
 import csv
 import pickle
 import numpy as np
+import argparse
 
 from block_utils import Object, Dimensions, Color, Position
 
@@ -10,23 +11,29 @@ def string_to_list(string):
     list_float = [float(s) for s in list_string if s != '']
     return list_float
 
-def main():
+def main(args):
     block_set = []
-    with open('learning/domains/towers/final_block_params.csv') as csv_file:
+    with open(args.csv_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row_i, row in enumerate(csv_reader):
             if row_i > 0:
                 id, dimensions, com, mass = row
                 
                 block = Object('obj_'+id,
-                                Dimensions(*string_to_list(dimensions)),
-                                float(mass),
-                                Position(*string_to_list(com)),
+                                Dimensions(*np.multiply(string_to_list(dimensions), .01)), # g --> kg
+                                float(mass)*.001, # g --> kg
+                                Position(*np.multiply(string_to_list(com), .01)), # cm --> m
                                 Color(*np.random.rand(3)))
+                print(block.dimensions, block.mass, block.com)
                 block_set.append(block)
             
-    with open('learning/domains/towers/final_block_set_12.pkl', 'wb') as f:
+    pkl_filename = args.csv_file[:-4]+'.pkl'
+    with open(pkl_filename, 'wb') as f:
         pickle.dump(block_set, f)
         
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv-file', type=str, required=True)
+    args = parser.parse_args()
+
+    main(args)
