e94d7d7dcaa3872f651f26f68c872754d8232629
Caris Moses <carism@mit.edu>
2021-02-13 13:44:06 -0500

    adding file for combining planning results accross methods

---
+++ learning/evaluate/plot_combine_plan_evaluate_models.py
@@ -0,0 +1,85 @@
+import argparse
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from learning.active.utils import ActiveExperimentLogger
+from learning.evaluate.active_evaluate_towers import plot_tallest_tower_regret
+from learning.evaluate.plot_plan_evaluate_models import plot_planner_performance
+
+def plot_all_task_performance(xs, plot_task_data, args, task):
+    fig, axes = plt.subplots(len(args.tower_sizes), figsize=(7, 15))
+    for mi, (method, method_plot_data) in enumerate(plot_task_data.items()):
+        for ai, (tower_size, plot_data) in enumerate(method_plot_data.items()):
+            axes[ai].plot(xs, plot_data['median'], label=method)
+            axes[ai].fill_between(xs, plot_data['lower25'], plot_data['upper75'], alpha=0.2)
+            axes[ai].set_ylim(0, 1)
+            axes[ai].set_ylabel('Regret')
+            axes[ai].set_xlabel('Number of Training Towers')
+            axes[ai].legend()
+            
+    fig.suptitle(task)
+    plot_dir = 'learning/experiments/logs/paper_plots/compare_methods/'
+    if not os.path.exists(plot_dir): os.makedirs(plot_dir)
+    plt.savefig('learning/experiments/logs/paper_plots/compare_methods/'+task+'.png')
+    plt.close()
+    
+    
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp-path-roots', 
+                        nargs='+',
+                        required=True)
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='set to run in debug mode')
+    parser.add_argument('--tower-sizes',
+                        default=[2, 3, 4, 5],
+                        nargs = '+',
+                        help='number of blocks in goal tower')
+    parser.add_argument('--max-acquisitions',
+                        type=int, 
+                        help='evaluate from 0 to this acquisition step (use either this or --acquisition-step)')
+    
+    args = parser.parse_args()
+    
+    args.tower_sizes = [int(ts) for ts in args.tower_sizes]
+    
+    if args.debug:
+        import pdb; pdb.set_trace()
+ 
+    tallest_tower_plot_data = {}
+    min_contact_plot_data = {}
+    max_overhang_plot_data = {}
+    all_plot_data = [tallest_tower_plot_data, min_contact_plot_data, max_overhang_plot_data]
+    y_axis = 'Regret' # TODO: detect from file name?
+    for exp_path_root in args.exp_path_roots:
+        # find exp_paths with the given root in paper_results/
+        results_path = 'paper_results'
+        exp_path_roots = [exp_path_root+'-'+str(r) for r in [0, 1, 2, 3, 4]]
+        all_paper_results = os.listdir(results_path)
+        exp_paths = []
+        for result in all_paper_results:
+            for exp_path_root in exp_path_roots:
+                if exp_path_root in result:
+                    exp_paths.append(result)
+        print(exp_paths)
+        
+        loggers = []
+        for exp_path in exp_paths:
+            loggers.append(ActiveExperimentLogger(os.path.join(results_path, exp_path)))
+            
+        
+        label = exp_path_root
+        fnames = ['random_planner_tallest2345_block_towers_regrets.pkl', \
+                    'random_planner_min_contact_2345_block_towers_regrets.pkl', \
+                    'random_planner_max_overhang_2345_block_towers_regrets.pkl']
+        for fname, task_plot_data in zip(fnames, all_plot_data):
+            xs, plot_data = plot_planner_performance(loggers, args, y_axis, label, fname)
+            task_plot_data[label] = plot_data
+            
+    tasks = ['Tallest Tower', 'Minimum Contact', 'Maximum Overhang']
+    for task, task_plot_data in zip(tasks, all_plot_data):
+        plot_all_task_performance(xs, task_plot_data, args, task)
--- learning/evaluate/plot_plan_evaluate_models.py
+++ learning/evaluate/plot_plan_evaluate_models.py
@@ -2,6 +2,7 @@ import argparse
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from learning.active.utils import ActiveExperimentLogger
 from learning.evaluate.active_evaluate_towers import plot_tallest_tower_regret
@@ -11,60 +12,72 @@ min_towers_acq = 40         # number of towers in initial dataset
 towers_per_acq = 10         # number of towers acquired between each trained model
 acquisition_step_size = 10  # steps between models evaluated
 
-def plot_planner_performance(logger, fname, args, y_axis, title):
-    fig, ax = plt.subplots()
-
-    with open(logger.get_figure_path(fname), 'rb') as handle:
-        metric = pickle.load(handle)
-    rs = metric[str(args.tower_size)+'block']
-    n_towers = len(rs[0])
+def plot_planner_performance(loggers, args, y_axis, title, fname):
+    all_rs = {str(ts)+'block' : [] for ts in args.tower_sizes}
+    for logger in loggers:
+        for tower_size in args.tower_sizes:
+            with open(logger.get_figure_path(fname), 'rb') as handle:
+                metric = pickle.load(handle)
+            try:
+                tower_size_key = str(tower_size)+'block'
+                rs = metric[tower_size_key]
+            except:
+                print('tower sizes '+str(tower_size)+' are not in file '+fname)
+            all_rs[tower_size_key].append(rs)
     
-    acquisition_plot_steps = len(range(0, args.max_acquisitions, acquisition_step_size))
-    txs = list(range(0, n_towers*acquisition_plot_steps, n_towers)) # indices into lists
+    plot_data = {}
+    for tsk in all_rs:
+        n_towers = len(all_rs[tsk][0][0])
+        acquisition_plot_steps = len(range(0, args.max_acquisitions, acquisition_step_size))
+        txs = list(range(0, n_towers*acquisition_plot_steps, n_towers)) # indices into lists
+
+        median, lower25, upper75 = [], [], []
+        for tx in range(len(all_rs[tsk][0])):
+            tx_rs = []
+            for rs in all_rs[tsk]:
+                tx_rs.append(rs[tx])
+            median.append(np.median(tx_rs))
+            lower25.append(np.quantile(tx_rs, 0.25))
+            upper75.append(np.quantile(tx_rs, 0.75))
+             
+        plot_data[tsk] = {}
+        plot_data[tsk]['median'] = median
+        plot_data[tsk]['lower25'] = lower25
+        plot_data[tsk]['upper75'] = upper75
+
     xs = np.arange(min_towers_acq, \
                     min_towers_acq+towers_per_acq*acquisition_step_size*acquisition_plot_steps, \
                     towers_per_acq*acquisition_step_size) # number of training towers
 
-    median, lower25, upper75 = [], [], []
-    for tx in range(len(rs)):
-        median.append(np.median(rs[tx]))
-        lower25.append(np.quantile(rs[tx], 0.25))
-        upper75.append(np.quantile(rs[tx], 0.75))
-         
-    ax.plot(xs, median)
-    ax.fill_between(xs, lower25, upper75, alpha=0.2)
-     
-    ax.set_ylabel(y_axis)
-    ax.set_xlabel('Number of Training Towers')
-    ax.set_title(title)
-    #ax.legend()
-    plt_fname = fname[:-4]+'.png'
-    plt.savefig(logger.get_figure_path(plt_fname))
-
+    fig, axes = plt.subplots(len(args.tower_sizes), figsize=(7, 15))
+    for i, (tsk, plot_data_tsk) in enumerate(plot_data.items()):
+        axes[i].plot(xs, plot_data_tsk['median'], label=tsk)
+        axes[i].fill_between(xs, plot_data_tsk['lower25'], plot_data_tsk['upper75'], alpha=0.2)
+        axes[i].set_ylim(0, 1)
+        axes[i].set_ylabel(y_axis)
+        axes[i].set_xlabel('Number of Training Towers')
+        axes[i].legend()
+        
+    fig.suptitle(title)
+    plot_dir = 'learning/experiments/logs/paper_plots/combine_models/'+title
+    if not os.path.exists(plot_dir): os.makedirs(plot_dir)
+    plt.savefig('learning/experiments/logs/paper_plots/combine_models/'+title+'/'+fname[:-4]+'.png')
     plt.close()
+    
+    return xs, plot_data
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--problem', 
-                        choices=['tallest', 'overhang', 'min-contact', 'deconstruct'], 
-                        default='tallest',
-                        help='planning problem/task to plan for')
-    parser.add_argument('--exp-path', 
-                        type=str, 
+    parser.add_argument('--exp-path-root', 
+                        type=str,
                         required=True)
     parser.add_argument('--debug',
                         action='store_true',
                         help='set to run in debug mode')
     parser.add_argument('--tower-sizes',
-                        default=[5],
+                        default=[2, 3, 4, 5],
                         nargs = '+',
                         help='number of blocks in goal tower')
-    parser.add_argument('--discrete',
-                        action='store_true',
-                        help='use if you want to ONLY search the space of block orderings and orientations')
-    parser.add_argument('--plot-type',
-                        default='regret',
-                        choices=['regret', 'reward'])
     parser.add_argument('--max-acquisitions',
                         type=int, 
                         help='evaluate from 0 to this acquisition step (use either this or --acquisition-step)')
@@ -76,25 +89,25 @@ if __name__ == '__main__':
     if args.debug:
         import pdb; pdb.set_trace()
  
-    logger = ActiveExperimentLogger(args.exp_path)
-    fname = 'discrete_' if args.discrete else ''
+    # find exp_paths with the given root in paper_results/
+    results_path = 'paper_results'
+    exp_path_roots = [args.exp_path_root+'-'+str(r) for r in [0, 1, 2, 3, 4]]
+    all_paper_results = os.listdir(results_path)
+    exp_paths = []
+    for result in all_paper_results:
+        for exp_path_root in exp_path_roots:
+            if exp_path_root in result:
+                exp_paths.append(result)
+    print(exp_paths)
     
-    ts_str = ''.join([str(ts) for ts in args.tower_sizes])
-    if args.problem == 'tallest':
-        fname += 'random_planner_tallest_tower_'+ts_str+'_block_towers'
-        title = 'Tallest Tower'
-    elif args.problem == 'overhang':
-        fname += 'random_planner_max_overhang_'+ts_str+'_block_towers'
-        title = 'Maximum Overhang'
-    elif args.problem == 'min-contact':
-        fname += 'random_planner_min_contact_'+ts_str+'_block_towers'
-        title = 'Minimum Contact'
+    loggers = []
+    for exp_path in exp_paths:
+        loggers.append(ActiveExperimentLogger(os.path.join(results_path, exp_path)))
         
-    if args.plot_type == 'regret':
-        fname += '_regrets.pkl'
-        y_axis = 'Normalized Median Regret'
-    elif args.plot_type == 'reward':
-        fname += '_rewards.pkl'
-        y_axis = 'Normalized Median Reward'
-        
-    plot_planner_performance(logger, fname, args, y_axis, title)
+    y_axis = 'Regret' # TODO: detect from file name?
+    title = args.exp_path_root
+    fnames = ['random_planner_tallest_2345_block_towers_regrets.pkl', \
+                'random_planner_min_contact_2345_block_towers_regrets.pkl', \
+                'random_planner_max_overhang_2345_block_towers_regrets.pkl']
+    for fname in fnames:
+        plot_planner_performance(loggers, args, y_axis, title, fname)
