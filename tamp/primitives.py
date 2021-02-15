d311506a7d929be10147d7ec56f6fd6c48e6b12c
Michael Noseworthy <mnoseworthy7@gmail.com>
2021-02-15 08:50:07 -0500

    Fix planning bug

--- tamp/primitives.py
+++ tamp/primitives.py
@@ -159,7 +159,7 @@ def get_ik_fn(robot, fixed=[], num_attempts=4, approach_frame='gripper', backoff
         if not is_top_grasp and is_wrist_too_low:
             return None
         # If the block/gripper is in the storage area, don't use low grasps.
-        if grasp_worldF[2,3] < 0.1 and grasp_worldF[0,3] < 0.2:
+        if grasp_worldF[0,3] < 0.2:#grasp_worldF[2,3] < 0.1:
             return None
 
 
