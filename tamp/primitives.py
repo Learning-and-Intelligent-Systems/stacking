dee6baf1245ccd5e397504e864fd252b17fbc6d7
Michael Noseworthy <mnoseworthy7@gmail.com>
2021-03-08 14:59:37 -0500

    Speed up planning by constraining stream domains

--- tamp/primitives.py
+++ tamp/primitives.py
@@ -126,7 +126,7 @@ def get_stable_gen_block(fixed=[]):
     return fn
 
 
-def get_ik_fn(robot, fixed=[], num_attempts=4, approach_frame='gripper', backoff_frame='global', use_wrist_camera=False):
+def get_ik_fn(robot, fixed=[], num_attempts=10, approach_frame='gripper', backoff_frame='global', use_wrist_camera=False):
     def fn(body, pose, grasp, return_grasp_q=False, check_robust=False):
         obstacles = fixed + [body]
         obj_worldF = pb_robot.geometry.tform_from_pose(pose.pose)
@@ -209,12 +209,15 @@ def get_ik_fn(robot, fixed=[], num_attempts=4, approach_frame='gripper', backoff
             conf_backoff = pb_robot.vobj.BodyConf(robot, q_backoff)
 
             path_approach = robot.arm.snap.PlanToConfiguration(robot.arm, q_approach, q_grasp, obstacles=obstacles)
+            if path_approach is None:
+                if DEBUG_FAILURE: input('Approach motion failed')
+                continue
             if backoff_frame == 'global':
                 path_backoff = robot.arm.snap.PlanToConfiguration(robot.arm, q_grasp, q_backoff, obstacles=obstacles, check_upwards=True)
             else:
                 path_backoff = robot.arm.snap.PlanToConfiguration(robot.arm, q_grasp, q_backoff, obstacles=obstacles, check_upwards=False)
-            if path_approach is None or path_backoff is None:
-                if DEBUG_FAILURE: input('Approach motion failed')
+            if path_backoff is None:
+                if DEBUG_FAILURE: input('Backoff motion failed')
                 continue
 
             # If the grasp is valid, check that it is robust (i.e., also valid under pose estimation error).
