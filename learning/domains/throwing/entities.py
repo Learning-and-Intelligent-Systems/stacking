f0b9b888d8e7172bed6a9fb40968f1e13b769a57
Izzy Brand <izzybrand@gmail.com>
2021-06-09 22:51:24 -0400

    add vector tools to ThrowingBall

--- learning/domains/throwing/entities.py
+++ learning/domains/throwing/entities.py
@@ -3,14 +3,21 @@ Description of entities for 2D throwing domain
 
 Copyright 2021 Massachusetts Institute of Technology
 """
+import numpy as np
+import scipy
+
+def truncnorm(lower, upper, mu, sigma, size=(1,)):
+    X = scipy.stats.truncnorm(
+    (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+    return X.rvs(size)
 
 class ThrowingBall:
     """ Represents a throwable ball with visual and physical properties """
     def __init__(self, color=[1,0,0], mass=1.0, radius=0.025,
                  air_drag_linear=1, air_drag_angular=1e-5,
-                 friction_coef=0.8, rolling_resistance=1e-4, 
+                 friction_coef=0.8, rolling_resistance=1e-4,
                  bounciness=0.5):
-        
+
         # Visual properties
         self.color = color
 
@@ -22,12 +29,54 @@ class ThrowingBall:
         # Air drag
         self.air_drag_linear = air_drag_linear
         self.air_drag_angular = air_drag_angular
-    
+
         # Friction, rolling, and bounce
         self.friction_coef = friction_coef
         self.rolling_resistance = rolling_resistance
         self.bounciness = bounciness
 
+
+    @staticmethod
+    def random():
+        color = np.random.rand(3)
+        mass = np.random.uniform(0.5, 1.5)
+        radius = np.random.uniform(0.2, 0.6)
+        air_drag_linear = np.random.uniform(0, 2)
+        air_drag_angular = np.random.uniform(5e-6, 5e-5)
+        friction_coef = np.random.uniform(0.1, 1)
+        rolling_resistance = np.random.uniform(1e-4, 1e-3)
+        bounciness = np.random.uniform(0.1, 0.6)
+        return ThrowingBall(color=color,
+                            mass=mass,
+                            radius=radius,
+                            air_drag_linear=air_drag_linear,
+                            air_drag_angular=air_drag_angular,
+                            friction_coef=friction_coef,
+                            rolling_resistance=rolling_resistance,
+                            bounciness=bounciness)
+
+    def vectorize(self):
+        v = np.zeros(10)
+        v[:3] = self.color
+        v[3] = self.mass
+        v[4] = self.radius
+        v[5] = self.air_drag_linear
+        v[6] = self.air_drag_angular
+        v[7] = self.friction_coef
+        v[8] = self.rolling_resistance
+        v[9] = self.bounciness
+        return v
+
+    @staticmethod
+    def from_vector(v):
+        return ThrowingBall(color=v[:3],
+                            mass=v[3],
+                            radius=v[4],
+                            air_drag_linear=v[5],
+                            air_drag_angular=v[6],
+                            friction_coef=v[7],
+                            rolling_resistance=v[8],
+                            bounciness=v[9])
 
 class ThrowingAction:
     """ Represents a throwing action """
