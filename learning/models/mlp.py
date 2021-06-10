8ca1f37fb96fc72b9b6f453e068e8622f9f78b55
Izzy Brand <izzybrand@gmail.com>
2021-06-10 11:11:53 -0400

    add general purpose feedforward network to models

--- learning/models/mlp.py
+++ learning/models/mlp.py
@@ -3,7 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 
 class MLP(nn.Module):
-    """ 
+    """
     Implements a MLP for a fixed number of blocks.
     """
 
@@ -31,3 +31,28 @@ class MLP(nn.Module):
         return torch.sigmoid(x)[...,0]
 
 
+class FeedForward(nn.Module):
+    """ general purpose feedforward NN
+
+    Extends:
+        nn.Module
+    """
+    def __init__(self, d_in, d_out, h_dims=[32, 32]):
+        super(FeedForward, self).__init__()
+        self.d_in = d_in
+        self.d_out = d_out
+        self.h_dims = h_dims
+        all_dims = [d_in] + list(h_dims) + [d_out]
+
+        # create a linear layer and nonlinearity for each hidden dim
+        modules = []
+        for i in range(len(all_dims) - 1):
+            modules.append(nn.Linear(all_dims[i], all_dims[i+1]))
+            modules.append(nn.LeakyReLU())
+
+        modules.pop(-1)  # don't include the last nonlinearity
+        self.layers = nn.Sequential(*modules)  # add modules to net
+
+    def forward(self, *xs):
+        x = torch.cat(xs, axis=1)
+        return self.layers(x)
