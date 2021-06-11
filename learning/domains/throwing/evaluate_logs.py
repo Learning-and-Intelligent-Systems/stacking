10b480b213404c20215f79fa4e822327decbcf76
Izzy Brand <izzybrand@gmail.com>
2021-06-11 16:49:59 -0400

    add validation accuracy plotting

--- learning/domains/throwing/evaluate_logs.py
+++ learning/domains/throwing/evaluate_logs.py
@@ -1,41 +1,69 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-
+from torch.utils.data import TensorDataset, DataLoader
 
 from learning.active.utils import ActiveExperimentLogger
 from learning.evaluate.active_evaluate_towers import plot_latent_uncertainty
+from learning.domains.throwing.train_latent import evaluate
+from learning.domains.throwing.throwing_data import generate_dataset
 from learning.domains.throwing.task import eval_hit_target
 from learning.domains.throwing.entities import ThrowingBall
 
 
 def plot_task_performance(logger, task_score_fn, ax=plt.gca()):
-	scores = []
-	for tx in range(logger.args.max_acquisitions):
-		latent_ensemble = logger.get_ensemble(tx)
-		score = task_score_fn(latent_ensemble)
-		print(f'Step {tx}. Score {score}')
-		scores.append(score)
+    scores = []
+    for tx in range(logger.args.max_acquisitions):
+        latent_ensemble = logger.get_ensemble(tx)
+        score = task_score_fn(latent_ensemble)
+        print(f'Step {tx}. Score {score}')
+        scores.append(score)
 
-	ax.plot(scores)
-	ax.set_xlabel('Acquisition Step')
-	ax.set_ylabel('Task Loss (meters to target)')
-	ax.set_title('Task Performance')
-	np.save(logger.get_results_path('task_performance.npy'), np.array(scores))
-	plt.savefig(logger.get_figure_path('task_performance.png'))
-	plt.clf()
+    ax.plot(scores)
+    ax.set_xlabel('Acquisition Step')
+    ax.set_ylabel('Task Loss (meters to target)')
+    ax.set_title('Task Performance')
+    np.save(logger.get_results_path('task_performance.npy'), np.array(scores))
+    plt.savefig(logger.get_figure_path('task_performance.png'))
+    plt.clf()
+
+
+def plot_val_accuracy(logger, n_data=200, ax=plt.gca()):
+    objects = logger.get_objects(ThrowingBall)
+
+    print('Generating a validation dataset')
+    val_dataset = TensorDataset(*generate_dataset(objects, n_data))
+    val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=64)
+
+    scores = []
+    for tx in range(logger.args.max_acquisitions):
+        latent_ensemble = logger.get_ensemble(tx)
+        score = evaluate(latent_ensemble, val_dataloader)
+        print(f'Step {tx}. Score {score}')
+        scores.append(score)
+
+    ax.plot(scores)
+    ax.set_xlabel('Acquisition Step')
+    ax.set_ylabel('Accuracy (likelihood of data)')
+    ax.set_title('Validation Accuracy')
+    np.save(logger.get_results_path('val_accuracy.npy'), np.array(scores))
+    plt.savefig(logger.get_figure_path('val_accuracy.png'))
+    plt.clf()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp-path', type=str, required=True)
+    parser.add_argument('--use-latents', action='store_true')
     args = parser.parse_args()
     
     logger = ActiveExperimentLogger(args.exp_path, use_latents=True)
-    logger.args.max_acquisitions = 50  # lazy
+    logger.args.max_acquisitions = 100  # lazy
     logger.args.throwing = True # lazy
 
     # plot_latent_uncertainty(logger)
+
+    # plot_val_accuracy(logger)
 
     objects = logger.get_objects(ThrowingBall)
     task_score_fn = lambda latent_ensemble: eval_hit_target(latent_ensemble, objects)
