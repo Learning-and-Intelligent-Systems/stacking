505badf91354abd57a7c98e31eb77fd051813bea
Caris M Moses <caris.moses@gmail.com>
2021-08-17 13:48:45 -0400

    adding more utils function and plotting there

--- learning/evaluate/utils.py
+++ learning/evaluate/utils.py
@@ -1,6 +1,7 @@
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
 
 from learning.domains.abc_blocks.world import LogicalState
 
@@ -85,3 +86,37 @@ def plot_horiz_bars(transitions, plot_inds, bar_text_inds, color=False, plot_tit
                 ha='left',
                 va='center',
                 rotation=0)
+
+def recc_dict():
+    return defaultdict(recc_dict)
+
+def plot_results(success_data, all_test_num_blocks, title, xlabel, ylabel, logger):
+    # plot colors
+    cs = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+
+    # plot all results
+    figure, axis = plt.subplots()
+    for i, (method, method_successes) in enumerate(success_data.items()):
+        method_avgs = []
+        method_mins = []
+        method_maxs = []
+        for test_num_blocks, num_block_successes in method_successes.items():
+            if method == 'OPT':
+                num_block_success_data = num_block_successes
+            else:
+                num_block_success_data = [data for model_path, data in num_block_successes.items()]
+            method_avgs.append(np.mean(num_block_success_data))
+            method_mins.append(np.mean(num_block_success_data)-np.std(num_block_success_data))
+            method_maxs.append(np.mean(num_block_success_data)+np.std(num_block_success_data))
+
+        axis.plot(all_test_num_blocks, method_avgs, color=cs[i], label=method)
+        axis.fill_between(all_test_num_blocks, method_mins, method_maxs, color=cs[i], alpha=0.1)
+
+    axis.set_xticks(all_test_num_blocks)
+    axis.set_title(title)
+    axis.set_xlabel(xlabel)
+    axis.set_ylabel(ylabel)
+    axis.legend(title='Method')
+
+    plt.savefig('%s/%s.png' % (logger.exp_path, title))
+    print('Saving figures to %s.' % logger.exp_path)
