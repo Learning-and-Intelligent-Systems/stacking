added normalization and fixed hide_dims bug

learning/domains/throwing/evaluate_logs.py
@@ -200,27 +200,21 @@ if __name__ == '__main__':
         #######################################################################
 
         runs = [
-            # {
-            #     "prefix": 'throwing_bald_sweep_run_',
-            #     "label": 'BALD',
-            #     "data": [],
-            #     "color": 'b'
-            # },
-            # {
-            #     "prefix": 'throwing_random_sweep_run_',
-            #     "label": 'Random',
-            #     "data": [],
-            #     "color": 'r'
-            # },
             {
-                "prefix": 'throwing_bald_sweep_fitting_run_',
+                "prefix": 'active',
                 "label": 'BALD',
                 "data": [],
                 "color": 'b'
             },
+            {
+                "prefix": 'random',
+                "label": 'Random',
+                "data": [],
+                "color": 'r'
+            },
 
         ]
-        exp_path = 'learning/experiments/logs/'
+        exp_path = 'learning/experiments/logs/new_throwing'
         ax = plt.gca()
         for r in runs:
             for fname in os.listdir(exp_path):
@@ -244,6 +238,7 @@ if __name__ == '__main__':
 
         for r in runs:
             plot_with_variance(np.arange(50), r["data"], ax, label=r["label"], c=r["color"])
+
         plt.legend()
         plt.xlabel('Acquisition Step')
         plt.ylabel('Task Error (m)')

learning/domains/throwing/new_sim.py
@@ -12,9 +12,10 @@ class ThrowingSimulator:
         self.obstacles = np.array([[0.06, 1, 0.06, 0.9, 0, 0.03]]) # dimensions || position
 
         self.stop_vel_thresh = 0.01      # Maximum linear velocity before stopping simulation [m/s]
-        self.stop_vel_count = 10         # Number of consecutive counts below velocity threshold before simulation is stopped 
+        self.stop_vel_count = 10         # Number of consecutive counts below velocity threshold before simulation is stopped
 
         self.setup()
+        self.trace = False
 
     def setup(self):
         # set up the simulator
@@ -53,7 +54,7 @@ class ThrowingSimulator:
         b = action.object
 
         # create a sphere
-        ballId = p.createMultiBody(b.mass, 
+        ballId = p.createMultiBody(b.mass,
             p.createCollisionShape(p.GEOM_SPHERE, radius=b.radius),
             p.createVisualShape(p.GEOM_SPHERE, radius=b.radius, rgbaColor=list(b.color) + [1]))
 
@@ -77,11 +78,16 @@ class ThrowingSimulator:
         }
 
         stop_count = 0
+        trace_ids = []
 
         for t in np.arange(0, self.tmax, dt):
             p.stepSimulation()
             results["state"].append(self.get_state(p, ballId))
             results["time"].append(t)
+
+            if self.trace:
+                trace_ids.append(p.createMultiBody(0, baseVisualShapeIndex=p.createVisualShape(p.GEOM_SPHERE, radius=0.01, rgbaColor=[1,0,0,0.2]),
+                    basePosition=p.getBasePositionAndOrientation(ballId)[0]))
 
             v = np.linalg.norm(results["state"][-1][3:5])
             if v < self.stop_vel_thresh:
@@ -95,6 +101,8 @@ class ThrowingSimulator:
             if self.vis: time.sleep(dt)
 
         p.removeBody(ballId)
+        for t_id in trace_ids:
+            p.removeBody(t)
 
         results["time"] = np.array(results["time"]).T
         results["state"] = np.array(results["state"]).T

learning/domains/throwing/particle_filter.py
@@ -27,14 +27,8 @@ def get_particle_likelihoods(dataloader, latent_ensemble, hide_dims):
             # we take in a ParallelDataloader to match the typical
             # train_latent.train spec, but we don't need all the
             # parallel batches
-            x, z_id, y = set_of_batches[0]
-            x = make_x_partially_observable(x, hide_dims)
+            x, z_id, y = preprocess_batch(set_of_batches[0], hide_dims)
             N_batch = x.shape[0]
-
-            if torch.cuda.is_available():
-                x = x.cuda()
-                z_id = z_id.cuda()
-                y = y.cuda()
 
             # pred should be [N_batch x N_particles x 2*D_output]
             pred = latent_ensemble(x, z_id.long(),
@@ -75,7 +69,7 @@ def resample(particles, weights):
     # cov = np.cov(resampled_particles, rowvar=False) + np.eye(D)*0.5
     # proposed_particles = np.random.multivariate_normal(mean=mean, cov=cov, size=N)
 
-    
+
 
 def update_particle_filter(dataloader, val_dataloader, latent_ensemble, n_epochs=30,
     freeze_latents=False,
@@ -91,7 +85,7 @@ def update_particle_filter(dataloader, val_dataloader, latent_ensemble, n_epochs
         for epoch_idx in range(n_epochs):
             print(f'Epoch {epoch_idx}')
             particle_likelihoods = get_particle_likelihoods(dataloader, latent_ensemble, hide_dims)
-     
+
             # resample the each latent variable individually
             for i in range(latent_ensemble.n_latents):
                 particles = latent_ensemble.latent_locs[i].detach().numpy()

learning/domains/throwing/sanity_checking/compare_training_runs.py
@@ -14,36 +14,36 @@ args.n_val = 100
 args.n_epochs = 500
 args.n_objects = 10
 
-labels = ['fully_observed', 'mass_hidden', 'radius_hidden', 'mass_and_radius_hidden']
-hide_dims = ['', '3', '4', '3,4']
+labels = ['fully_observed', 'restitution_hidden', 'radius_hidden', 'restitution_and_radius_hidden']
+hide_dims = ['', '9', '4', '9,4']
 
 processes = []
 
-n_max = 5
+n_max = 3
 d_max = 3
 
-# # execute the commands in parallel using multiprocess
-# for n in range(n_max):
-#     for l, h in zip(labels, hide_dims):
-#         for d in range(d_max):
-#             args.d_latent = d
-#             args.hide_dims = h
-#             args.save_accs = f"learning/domains/throwing/sanity_checking/data/500_epochs/{l}_{d}d_latent_run_{n}.npy"
+# execute the commands in parallel using multiprocess
+for n in range(n_max):
+    for l, h in zip(labels, hide_dims):
+        for d in range(d_max):
+            args.d_latent = d
+            args.hide_dims = h
+            args.save_accs = f"learning/domains/throwing/sanity_checking/data/500_epochs/{l}_{d}d_latent_run_{n}.npy"
 
-#             # main(args)
-#             if len(processes) > 13:
-#                 print('[WARNING] Waiting for a process to complete!')
-#                 processes[0].join()
-#                 processes.pop(0)
+            # main(args)
+            if len(processes) > 13:
+                print('[WARNING] Waiting for a process to complete!')
+                processes[0].join()
+                processes.pop(0)
 
-#             processes.append(Process(target=main, args=(deepcopy(args),)))
-#             np.random.seed() # this is critical!
-#             torch.random.seed() # this is critical!
-#             processes[-1].start()
+            processes.append(Process(target=main, args=(deepcopy(args),)))
+            np.random.seed() # this is critical!
+            torch.random.seed() # this is critical!
+            processes[-1].start()
 
 
-# for p in processes:
-#     p.join()
+for p in processes:
+    p.join()
 
 
 

learning/domains/throwing/simulator.py
@@ -14,7 +14,7 @@ class ThrowingSimulator:
     gravity = 9.80665           # Acceleration due to gravity [m/s^2]
     bounce_vel_thresh = -0.05   # Threshold velocity before stopping bouncing [m/s]
     stop_vel_thresh = 0.01      # Maximum linear velocity before stopping simulation [m/s]
-    stop_vel_count = 10         # Number of consecutive counts below velocity threshold before simulation is stopped 
+    stop_vel_count = 10         # Number of consecutive counts below velocity threshold before simulation is stopped
 
 
     def __init__(self, objects, dt=0.0005, tmax=5):
@@ -54,7 +54,7 @@ class ThrowingSimulator:
         for i in range(num_pts-1):
             x, y, theta, vx, vy, w = state[:,i]
             forces = np.array([0.0, 0.0, 0.0])
-            
+
             # Gravity force
             forces += [
                 0,
@@ -75,10 +75,10 @@ class ThrowingSimulator:
                 v_ground = vx + b.radius*w
                 f_friction = b.friction_coef * weight
                 forces += [
-                    -np.sign(v_ground) * f_friction, 
+                    -np.sign(v_ground) * f_friction,
                     0,
                     -b.rolling_resistance * w,
-                ] 
+                ]
 
             # Propagate state with Newton's 2nd law and first-order integration step
             state_dot = np.array([
@@ -93,7 +93,7 @@ class ThrowingSimulator:
 
             # Bounce/collision dynamics:
             # If the height is less than zero, force it to zero.
-            # Also, correct velocity by bouncing if past a specified velocity 
+            # Also, correct velocity by bouncing if past a specified velocity
             # threshold, else setting the vertical velocity to zero.
             if state[1,i+1] < 0:
                 state[1,i+1] = 0
@@ -125,7 +125,7 @@ class ThrowingSimulator:
             self.animate_results(b, state)
         elif do_plot:
             self.plot_results(b, state)
-            
+
         # Package results
         results = {
             "time": tvec,
@@ -154,10 +154,10 @@ class ThrowingSimulator:
         ax.add_patch(circ_final)
 
         ori_line_init, = ax.plot([x[0], x[0] + obj.radius*np.cos(th[0])],
-                                 [y[0], y[0] + obj.radius*np.sin(th[0])], 
+                                 [y[0], y[0] + obj.radius*np.sin(th[0])],
                                  color="k")
         ori_line_final, = ax.plot([x[-1], x[-1] + obj.radius*np.cos(th[-1])],
-                                 [y[-1], y[-1] + obj.radius*np.sin(th[-1])], 
+                                 [y[-1], y[-1] + obj.radius*np.sin(th[-1])],
                                  color="k")
 
         ax.set_xlabel('Distance (m)')
@@ -207,7 +207,7 @@ class ThrowingSimulator:
             x_ori = [x, x + obj.radius*np.cos(th)]
             y_ori = [y, y + obj.radius*np.sin(th)]
             ori_line.set_data(x_ori, y_ori)
-            
+
             # Close the figure if this is the last step
             # NOTE: This is not reliable, and will lead to a core dump if
             # you use blit=True in animation.FuncAnimation
@@ -221,6 +221,6 @@ class ThrowingSimulator:
         pts_skip = int(dt/self.dt)
         frames = np.arange(0, state.shape[1], pts_skip)
         ani = animation.FuncAnimation(
-            fig, animate, init_func=init, frames=frames, 
+            fig, animate, init_func=init, frames=frames,
             blit=False, repeat=False, interval=dt*1000)
         plt.show()

learning/domains/throwing/throwing_data.py
@@ -6,6 +6,10 @@ from torch.utils.data import DataLoader
 from agents.throwing_agent import ThrowingAgent
 from learning.domains.throwing.entities import ThrowingBall, ThrowingAction
 
+###############################################################################
+# dataset generation
+###############################################################################
+
 def sample_actions(obj_ids, n_samples=1):
     """ sample which object to throw, and the parameters of that throw
     """
@@ -34,7 +38,7 @@ def construct_xs(objects, actions, z_ids):
     for a, z_id in zip(actions, z_ids):
         b = objects[z_id]
         v = b.vectorize()
-        x = np.concatenate([a, v])
+        x = np.concatenate([v, a])
         xs.append(x)
 
     return np.array(xs)
@@ -50,7 +54,7 @@ def generate_dataset(objects,
     actions, z_ids = sample_actions(obj_ids, n_samples=n_data)
     xs = construct_xs(objects, actions, z_ids)
     if label:
-        ys = label_actions(objects, actions, z_ids)
+        ys = label_actions(objects, actions, z_ids, as_tensor=as_tensor)
         dataset = xs, z_ids, ys
     else:
         dataset = xs, z_ids
@@ -61,7 +65,7 @@ def generate_dataset_parallel(objects,
                               as_tensor=True,
                               label=True,
                               n_workers=8):
-    
+
     n_data_per_process = int(np.ceil(n_data/n_workers))
     processes = [Process(target=generate_dataset,
                          args=(objects, n_data_per_process),
@@ -79,10 +83,72 @@ def generate_dataset_parallel(objects,
 def generate_objects(n_objects):
     return [ThrowingBall.random() for _ in range(n_objects)]
 
+###############################################################################
+# data pre/post processing for NN training
+###############################################################################
+
 def make_x_partially_observable(xs, hide_dims):
+    # the fully observed dimension is the ball parameters and the throw parameters
     fully_obs_dim = ThrowingBall.dim + ThrowingAction.dim
     keep_dims = list(set(np.arange(fully_obs_dim)).difference(set(hide_dims)))
     return xs[..., keep_dims]
+
+def normalize_x(x):
+    x_mean = torch.Tensor([ 1.,  0.,  0.,  1.,
+        4.0470269e-02,  1.,  1e-5,  0.8,
+        1e-4,  4.3595454e-01,  7.8840643e-01, -3.1721351e-01])
+    x_var = torch.Tensor([0., 0., 0., 0.,
+       1.2081314e-04, 0., 0., 0.,
+       0., 4.1699380e-02, 5.3409986e-02, 3.3062962e+01])
+
+    return (x - x_mean)/torch.sqrt(x_var + 1e-6)
+
+def normalize_y(y):
+    y_mean = 1.0001804
+    y_var = 0.21319202
+    return (y - y_mean)/np.sqrt(y_var)
+
+def unnormalize_y(y):
+    y_mean = 1.0001804
+    y_var = 0.21319202
+    return y * np.sqrt(y_var) + y_mean
+
+def preprocess_batch(batch, hide_dims, normalize):
+    """ batch preprocessing before feeding into the NN """
+    x = batch[0]
+    z_id = batch[1]
+    y = batch[2] if len(batch) == 3 else None
+
+    if torch.cuda.is_available():
+        x = x.cuda()
+        z_id = z_id.cuda()
+        if y is not None:
+            y = y.cuda()
+
+    if normalize:
+        x = normalize_x(x)
+        if y is not None:
+            y = normalize_y(y)
+
+    x = make_x_partially_observable(x, hide_dims)
+
+    if y is not None:
+        return x, z_id, y
+    else:
+        return x, z_id
+
+def postprocess_pred(pred, unnormalize):
+    D_pred = pred.shape[-1] // 2
+    mu, log_sigma = torch.split(pred, D_pred, dim=-1)
+    sigma = torch.exp(log_sigma)
+    if unnormalize:
+        return unnormalize_y(mu), unnormalize_y(sigma)
+    else:
+        return mu, sigma
+
+###############################################################################
+# dataloader for training ensembles
+###############################################################################
 
 class ParallelDataLoader:
     def __init__(self, dataset, batch_size, shuffle, n_dataloaders=1):

learning/domains/throwing/train_latent.py
@@ -11,7 +11,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from learning.models.ensemble import Ensemble
 from learning.models.mlp import FeedForward
 from learning.models.latent_ensemble import ThrowingLatentEnsemble
-from learning.domains.throwing.throwing_data import generate_objects, generate_dataset, ParallelDataLoader, make_x_partially_observable
+from learning.domains.throwing.throwing_data import generate_objects, generate_dataset, ParallelDataLoader, preprocess_batch, postprocess_pred
 
 # NOTE(izzy): work in progress -- the following three functions have very similar
 # structure, so I wanted to consolidate them in this one function
@@ -22,15 +22,12 @@ from learning.domains.throwing.throwing_data import generate_objects, generate_d
 #             marginalize_latents=True,
 #             marginalize_ensemble=True,
 #             mode='train',
-#             hide_dims=[3]):
+#             hide_dims=[]):
 
 #         # pull out the input
-#         x = batch[0]
-#         x = make_x_partially_observable(x, hide_dims)
-#         z_id = batch[1]
-#         if torch.cuda.is_available():
-#             x = x.cuda()
-#             z_id = z_id.cuda()
+#           batch = preprocess_batch(batch, hide_dims)
+#           x = batch[0]
+#           z_id = batch[1]
 
 #         with torch.set_grad_enabled(mode=='train'): # conditional "torch.no_grad"
 #             pred = latent_ensemble(x, z_id.long(),
@@ -59,7 +56,8 @@ def get_predictions(latent_ensemble,
                     n_latent_samples=10,
                     marginalize_latents=True,
                     marginalize_ensemble=True,
-                    hide_dims=[3]):
+                    hide_dims=[],
+                    use_normalization=True):
 
     dataset = TensorDataset(*unlabeled_data)
     dataloader = DataLoader(dataset, shuffle=False, batch_size=64)
@@ -68,11 +66,7 @@ def get_predictions(latent_ensemble,
     sigmas = []
 
     for batch in dataloader:
-        x, z_id = batch
-        x = make_x_partially_observable(x, hide_dims)
-        if torch.cuda.is_available():
-            x = x.cuda()
-            z_id = z_id.cuda()
+        x, z_id = preprocess_batch(batch, hide_dims, normalize=use_normalization)
 
         with torch.no_grad():
             # run a forward pass of the network and compute the likeliehood of y
@@ -80,9 +74,8 @@ def get_predictions(latent_ensemble,
                                    collapse_latents=marginalize_latents,
                                    collapse_ensemble=marginalize_ensemble,
                                    N_samples=n_latent_samples).squeeze()
-            D_pred = pred.shape[-1] // 2
-            mu, log_sigma = torch.split(pred, D_pred, dim=-1)
-            sigma = torch.exp(log_sigma)
+
+            mu, sigma = postprocess_pred(pred, unnormalize=use_normalization)
 
         mus.append(mu)
         sigmas.append(sigma)
@@ -93,7 +86,8 @@ def get_both_loss(latent_ensemble,
                   batches,
                   N,
                   N_samples=10,
-                  hide_dims=[3]):
+                  hide_dims=[],
+                  use_normalization=True):
     """ compute the loglikelohood of both the latents and the ensemble
 
     Arguments:
@@ -110,22 +104,14 @@ def get_both_loss(latent_ensemble,
     loss_func = nn.GaussianNLLLoss(reduction='sum', full=True)
 
     for i, batch in enumerate(batches):
-        x, z_id, y = batch
-        x = make_x_partially_observable(x, hide_dims)
+        x, z_id, y = preprocess_batch(batch, hide_dims, normalize=use_normalization)
         N_batch = x.shape[0]
 
-        if torch.cuda.is_available():
-            x = x.cuda()
-            z_id = z_id.cuda()
-            y = y.cuda()
-
-        # run a forward pass of the network and compute the likeliehood of y
-
+        # run a forward pass of the network
         pred = latent_ensemble(x, z_id.long(), ensemble_idx=i, collapse_latents=False, collapse_ensemble=False, N_samples=N_samples).squeeze(dim=1)
-        D_pred = pred.shape[-1] // 2
-        mu, log_sigma = torch.split(pred, D_pred, dim=-1)
-        sigma = torch.exp(log_sigma)
 
+        # and compute the likeliehood of y (no need to un-normalize, because label will already be normalized)
+        mu, sigma = postprocess_pred(pred, unnormalize=False)
         try:
             likelihood_loss += loss_func(y[:, None, None].expand(N_batch, N_samples, 1),
                                          mu, sigma)
@@ -145,7 +131,8 @@ def get_both_loss(latent_ensemble,
 
 def evaluate(latent_ensemble,
             dataloader,
-            hide_dims=[3]):
+            hide_dims=[],
+            use_normalization=True):
     """ computes the data likelihood
 
     Arguments:
@@ -165,27 +152,27 @@ def evaluate(latent_ensemble,
     N = dataloader.dataset.tensors[0].shape[0]
 
     for batches in dataloader:
-        x, z_id, y = batches[0] if isinstance(dataloader, ParallelDataLoader) else batches
-        x = make_x_partially_observable(x, hide_dims)
-        if torch.cuda.is_available():
-            x = x.cuda()
-            z_id = z_id.cuda()
-            y = y.cuda()
+        # pull out a batch
+        batch = batches[0] if isinstance(dataloader, ParallelDataLoader) else batches
+        x, z_id, y = preprocess_batch(batch, hide_dims, normalize=use_normalization)
 
-        # run a forward pass of the network and compute the likeliehood of y
+        # run a forward pass of the network
         pred = latent_ensemble(x, z_id.long()).squeeze()
-        D_pred = pred.shape[-1] // 2
-        mu, log_sigma = torch.split(pred, D_pred, dim=-1)
-        total_log_prob += -loss_func(y, mu.squeeze(), torch.exp(log_sigma).squeeze())
+
+        # and compute the likelihood of y (no need to un-normalize, because label will already be normalized)
+        mu, sigma = postprocess_pred(pred, unnormalize=False)
+        total_log_prob += -loss_func(y, mu.squeeze(), sigma.squeeze())
 
     return total_log_prob / N
 
 
-def train(dataloader, val_dataloader, latent_ensemble, n_epochs=30,
+def train(dataloader, val_dataloader, latent_ensemble,
+    n_epochs=30,
     freeze_latents=False,
     freeze_ensemble=False,
     return_logs=False,
-    hide_dims=[3]):
+    hide_dims=[],
+    use_normalization=True):
 
     params_optimizer = optim.Adam(latent_ensemble.ensemble.parameters(), lr=1e-3)
     latent_optimizer = optim.Adam([latent_ensemble.latent_locs, latent_ensemble.latent_logscales], lr=1e-3)
@@ -201,7 +188,12 @@ def train(dataloader, val_dataloader, latent_ensemble, n_epochs=30,
         for batch_idx, set_of_batches in enumerate(dataloader):
             params_optimizer.zero_grad()
             latent_optimizer.zero_grad()
-            both_loss = get_both_loss(latent_ensemble, set_of_batches, N=len(dataloader.loaders[0].dataset), hide_dims=hide_dims)
+
+            both_loss = get_both_loss(latent_ensemble, set_of_batches,
+                N=len(dataloader.loaders[0].dataset),
+                hide_dims=hide_dims,
+                use_normalization=use_normalization)
+
             both_loss.backward()
             if not freeze_latents: latent_optimizer.step()
             if not freeze_ensemble: params_optimizer.step()
@@ -209,7 +201,10 @@ def train(dataloader, val_dataloader, latent_ensemble, n_epochs=30,
 
 
         if val_dataloader is not None:
-            val_acc = evaluate(latent_ensemble, val_dataloader, hide_dims=hide_dims)
+            val_acc = evaluate(latent_ensemble, val_dataloader,
+                hide_dims=hide_dims,
+                use_normalization=use_normalization)
+
             accs.append(val_acc.item())
             if val_acc > best_acc:
                 best_acc = val_acc
@@ -274,7 +269,7 @@ def generate_or_load_datasets(args):
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hide-dims', type=str, default='3')
+    parser.add_argument('--hide-dims', type=str, default='9')
     parser.add_argument('--train-dataset', type=str, default='')
     parser.add_argument('--val-dataset', type=str, default='')
     parser.add_argument('--save-train-dataset', type=str, default='')
@@ -287,6 +282,7 @@ def get_parser():
     parser.add_argument('--n-objects', type=int, default=10)
     parser.add_argument('--n-models', type=int, default=10)
     parser.add_argument('--d-latent', type=int, default=2)
+    parser.add_argument('--use-normalization', action='store_true')
 
     return parser
 
@@ -322,7 +318,8 @@ def main(args):
                                            latent_ensemble,
                                            n_epochs=args.n_epochs,
                                            return_logs=True,
-                                           hide_dims=hide_dims)
+                                           hide_dims=hide_dims,
+                                           use_normalization=args.use_normalization)
 
     if args.save_accs != "":
         print('Saving accuracy data to', args.save_accs)