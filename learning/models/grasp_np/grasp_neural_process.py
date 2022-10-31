9ac760a2536df57bb9b107d34dc1d90bed6ebc77
Michael Noseworthy <mnosew@mit.edu>
2022-10-31 16:14:54 -0400

    Remove ununsed experimental neural process code

--- learning/models/grasp_np/grasp_neural_process.py
+++ learning/models/grasp_np/grasp_neural_process.py
@@ -4,94 +4,6 @@ import torch.nn as nn
 
 from learning.models.pointnet import PointNetEncoder, PointNetRegressor, PointNetClassifier, PointNetPerPointClassifier
 
-class MultiTargetGraspNeuralProcess(nn.Module):
-
-    def __init__(self, d_latents):
-        super(MultiTargetGraspNeuralProcess, self).__init__()
-        self.encoder = GNPEncoder(d_latents=d_latents)
-        self.decoder = MultiTargetGNPDecoder(n_in=3+1+d_latents)
-        self.d_latents = d_latents
-        
-    def forward(self, contexts, target_xs):
-        mu, sigma = self.encoder(contexts)
-
-        # Sample via reparameterization trick.
-        q_z = torch.distributions.normal.Normal(mu, sigma)
-        z = q_z.rsample()[:, :, None].expand(-1, -1, target_xs.shape[-1])
-        
-        # Replace True properties with latent samples.
-        target_xs_with_latents = torch.cat([target_xs, z], dim=1)
-                
-        y_pred = self.decoder(target_xs_with_latents)
-        return y_pred, q_z
-
-class GraspNeuralProcess(nn.Module):
-
-    def __init__(self, d_latents):
-        super(GraspNeuralProcess, self).__init__()
-        self.encoder = GNPEncoder(d_latents=d_latents)
-        self.decoder = GNPDecoder(n_in=6+d_latents)
-        self.d_latents = d_latents
-        
-    def forward(self, contexts, target_xs, n_context=-1):
-        total_context_points = int(contexts[0, 3, :].sum().item())
-        assert(n_context <= total_context_points)
-        if n_context == -1:
-            n_context = 49 #np.random.randint(total_context_points)
-        
-        # Only keep specified amount of context points.
-        keep_ixs = torch.randperm(total_context_points)[:n_context]
-        contexts = torch.cat([
-            contexts[:, :, keep_ixs],
-            contexts[:, :, total_context_points:]
-        ], dim=2)
-
-        mu, sigma = self.encoder(contexts)
-
-        # Sample via reparameterization trick.
-        q_z = torch.distributions.normal.Normal(mu, sigma)
-        z = q_z.rsample()[:, :, None].expand(-1, -1, target_xs.shape[-1])
-        
-        # Replace True properties with latent samples.
-        target_xs_with_latents = torch.cat([target_xs[:, :-5, :], z], dim=1)
-                
-        y_pred = self.decoder(target_xs_with_latents)
-        return y_pred, q_z
-        
-class GNPEncoder(nn.Module):
-
-    def __init__(self, d_latents):
-        super(GNPEncoder, self).__init__()
-        self.pointnet = PointNetRegressor(n_in=5, n_out=d_latents*2)
-        self.d_latents = d_latents
-
-    def forward(self, contexts):
-        
-        x = self.pointnet(contexts)
-        mu, log_sigma = x[..., :self.d_latents], x[..., self.d_latents:]
-        sigma = 0.01 + 0.99 * torch.sigmoid(log_sigma)
-        #sigma = 0.01 + torch.exp(log_sigma)
-        return mu, sigma   
-        
-class GNPDecoder(nn.Module):
-
-    def __init__(self, n_in):
-        super(GNPDecoder, self).__init__()
-        self.pointnet = PointNetClassifier(n_in=n_in)
-
-    def forward(self, target_xs):
-        return self.pointnet(target_xs)
-
-class MultiTargetGNPDecoder(nn.Module):
-
-    def __init__(self, n_in):
-        super(MultiTargetGNPDecoder, self).__init__()
-        self.pointnet = PointNetPerPointClassifier(n_in=n_in)
-
-    def forward(self, target_xs):
-        return self.pointnet(target_xs)
-
-
 
 class CustomGraspNeuralProcess(nn.Module):
 
@@ -105,7 +17,7 @@ class CustomGraspNeuralProcess(nn.Module):
         
     def forward(self, contexts, target_xs, meshes):
         mesh_enc = self.mesh_encoder(meshes)
-        mesh_enc = torch.zeros_like(mesh_enc)
+        # mesh_enc = torch.zeros_like(mesh_enc)
         mu, sigma = self.encoder(*contexts, mesh_enc)
 
         # Sample via reparameterization trick.
@@ -153,7 +65,7 @@ class CustomGNPEncoder(nn.Module):
         super(CustomGNPEncoder, self).__init__()
 
         # Used to encode local geometry.
-        n_out_geom = 8
+        n_out_geom = 16
         self.pn_geom = PointNetRegressor(n_in=3, n_out=n_out_geom)
         self.pn_grasp = PointNetRegressor(n_in=3+1+n_out_geom+d_mesh, n_out=d_latents*2) # Input is grasp_midpoint, 
         self.d_latents = d_latents
